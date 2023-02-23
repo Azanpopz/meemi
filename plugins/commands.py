1dee7d7753bf87f645aa75acb8e25dafc8e887a1
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-23 19:18:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -550,7 +550,16 @@ async def settings(client, message):
                     callback_data=f'setgs#auto_delete#{settings["auto_delete"]}#{grp_id}',
                 ),
             ],
-
+            [
+                InlineKeyboardButton(
+                    'Aᴜᴛᴏ-Fɪʟᴛᴇʀ',
+                    callback_data=f'setgs#auto_ffilter#{settings["auto_ffilter"]}#{grp_id}',
+                ),
+                InlineKeyboardButton(
+                    '✔ Oɴ' if settings["auto_ffilter"] else '✘ Oғғ',
+                    callback_data=f'setgs#auto_ffilter#{settings["auto_ffilter"]}#{grp_id}',
+                ),
+            ],
             [
                 InlineKeyboardButton(
                     'Mᴀx Bᴜᴛᴛᴏɴs',
