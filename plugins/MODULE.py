01f3271deb3e53461e0dae76f4f2c3ec4926c7ad
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-24 00:40:26 +0530

    Update MODULE.py

--- plugins/MODULE.py
+++ plugins/MODULE.py
@@ -11,4 +11,4 @@ async def autoapprove(client, message: ChatJoinRequest):
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        await client.send_message(text=TEXT.format(mention=user.mention, title=chat.title))
