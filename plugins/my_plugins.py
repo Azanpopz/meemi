59ce69d69d86fe1dba17de7f297eefced32dae67
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-24 01:05:12 +0530

    Update my_plugins.py

--- plugins/my_plugins.py
+++ plugins/my_plugins.py
@@ -1,27 +1,15 @@
 import os
 import asyncio
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
-from info import CHAT_ID, APPROVED 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from info import CHAT_ID, TEXT, APPROVED 
 
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat.id 
     user=message.from_user 
-    print(f"{user.first_name} Joined") 
+    print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        buttons = [[
-            InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ​', url=f'https://t.me/nasrani_update')
-            
-        ]]
-        markup = InlineKeyboardMarkup(buttons)
-        caption = f"<b>Dᴇᴀʀ {message.from_user.mention()},\n\nYour Request To Jᴏɪɴ {message.chat.title} Was Approved</b>"
-        await client.send_photo(
-            message.from_user.id, 
-            photo='https://telegra.ph/file/13c860f7ef9f90e9e07cb.jpg', 
-            caption=caption, 
-            parse_mode=enums.ParseMode.HTML
-        )
+        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
