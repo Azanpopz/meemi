277b49f207a631054ccf8bfe50267c4b45098dd0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-23 18:51:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -22,6 +22,8 @@ from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_
 
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
+from database.ia import Media, get_file_details, get_search_results, get_bad_files
+
 from database.filters_mdb import (
     del_all,
     find_filter,
