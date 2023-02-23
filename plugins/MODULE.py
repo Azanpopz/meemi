2aab54eed6d7df5b42b1c4bf7bfccbdb8a7daf29
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-24 00:38:25 +0530

    Update MODULE.py

--- plugins/MODULE.py
+++ plugins/MODULE.py
@@ -6,8 +6,7 @@ from info import CHAT_ID, TEXT, APPROVED
 
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
-async def autoapprove(client, message: ChatJoinRequest):
-    chat=message.chat 
+async def autoapprove(client, message: ChatJoinRequest):     
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
