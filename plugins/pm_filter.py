710c97b9fd96446b9a923dec8728da7d3c4a1188
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-24 15:19:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1782,8 +1782,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
-            await client.send_message(
-                chat_id=chat.id,
+            await client.send_message(                
                 text=f"<b>Cʜᴀɴɢᴇ Yᴏᴜʀ Sᴇᴛᴛɪɴɢs Fᴏʀ {title} As Yᴏᴜʀ Wɪsʜ ⚙</b>",
                 reply_markup=reply_markup,
                 disable_web_page_preview=True,
