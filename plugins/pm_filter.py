374e2b727e91a963aae3ac7cdc1538f9b2d987fa
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-27 00:04:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -607,10 +607,7 @@ async def next_page(bot, query):
     try:
         await query.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(btn)
-        )
-    except MessageNotModified:
-        pass
-    await query.answer()
+        )    
     except MessageNotModified:
         pass
     await query.answer()
