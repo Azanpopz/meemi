a49769f9d5e415b556887c182715dcabf95ddae5
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-28 01:00:48 +0530

    Update remove_fwd_tag.py

--- plugins/myplugins/remove_fwd_tag.py
+++ plugins/myplugins/remove_fwd_tag.py
@@ -2,23 +2,29 @@ import pyrogram
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
+import os
+from pyrogram import Client, filters
 
-from info import BOT_TOKEN, API_ID, API_HASH
+from info import BOT_TOKEN, API_ID, API_HASH, CHANNELS
 
 
 Bot = Client(
-    "NoLink-BOT",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+     os.environ.get("SESSION_NAME", "No-Forward-Messages"),
+     bot_token = os.environ["BOT_TOKEN"],
+     api_id = int(os.environ["API_ID"]),
+     api_hash = os.environ["API_HASH"]
 )
-
 
 @Client.on_message(filters.forwarded & filters.channel & filters.group & filters.incoming)
 async def channel_tag(bot, message):
     try:
-        chat_id = message.chat.id
+        chat_id = CHANNELS
         forward_msg = await message.copy(chat_id)
         await message.delete()
     except:
         await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
+
+
+@Client.on_message(filters.forwarded)
+async def forward(bot, message):
+	await message.delete()
