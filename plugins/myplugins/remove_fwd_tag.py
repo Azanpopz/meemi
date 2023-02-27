875b7f1ba967439e66d3a0861234e8d14c33601d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-28 00:52:43 +0530

    Update remove_fwd_tag.py

--- plugins/myplugins/remove_fwd_tag.py
+++ plugins/myplugins/remove_fwd_tag.py
@@ -3,6 +3,16 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
+from info import BOT_TOKEN, API_ID, API_HASH
+
+
+Bot = Client(
+    "NoLink-BOT",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
 
 @Client.on_message(filters.forwarded & filters.channel & filters.group & filters.incoming)
 async def channel_tag(bot, message):
