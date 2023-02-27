314d62fa4dcd79aeafb59276f69711b48b592936
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-27 16:36:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1946,7 +1946,7 @@ async def advantage_spell_chok(client, msg):
     btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"▫️movie_name.strip()", callback_data=f"spol#{reqstr1}#{k}")
+                        text=f"▫️movie_name.strip()", callback_data=f"spol#{reqstr1}#{k}"),
                     )
                 ]
                 for k, movie_name in enumerate(movielist)
