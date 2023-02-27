187ea94f32f066f64582a7d364c0e118306678d0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-27 16:11:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1940,8 +1940,7 @@ async def advantage_spell_chok(client, msg):
     movielist += [movie.get('title') for movie in movies]
     movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
     SPELL_CHECK[mv_id] = movielist
-            else:
-                same = True
+            
         else:
             pre_len["text_len"] = len(text)
             same = False
