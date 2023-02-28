f391d55750655aa230cf8fc49c29e7a6bdf1ee2a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-28 16:50:46 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -10,18 +10,6 @@ logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
 
 
-
-import asyncio
-from pyrogram import Client, filters, enums
-from pyrogram.errors import QueryIdInvalid, FloodWait
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, InlineQuery, InlineQueryResultArticle, \
-    InputTextMessageContent
-
-
-
-
-
-
 async def inline_users(query: InlineQuery):
     if AUTH_USERS:
         if query.from_user and query.from_user.id in AUTH_USERS:
@@ -128,47 +116,3 @@ def get_reply_markup(query):
 
 
 
-@Client.on_inline_query()
-async def inline_handlers(_, inline: InlineQuery):
-    search_ts = inline.query
-    answers = []
-    if search_ts == "":
-        answers.append(
-            InlineQueryResultArticle(
-                title="Search Something ...",
-                description="Search For Torrents ...",
-                input_message_content=InputTextMessageContent(
-                    message_text="Search for Torrents from Inline!",
-                    parse_mode="Markdown"
-                ),
-                reply_markup=InlineKeyboardMarkup(DEFAULT_SEARCH_MARKUP)
-            )
-        )
-    elif search_ts.startswith("!pb"):
-        query = search_ts.split(" ", 1)[-1]
-        if (query == "") or (query == " "):
-            answers.append(
-                InlineQueryResultArticle(
-                    title="!pb [text]",
-                    description="Search For Torrent in ThePirateBay ...",
-                    input_message_content=InputTextMessageContent(
-                        message_text="`!pb [text]`\n\nSearch ThePirateBay Torrents from Inline!",
-                        parse_mode="Markdown"
-                    ),
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Search Again", switch_inline_query_current_chat="!pb ")]])
-                )
-            )
-        else:
-            torrentList = await SearchPirateBay(query)
-            if not torrentList:
-                answers.append(
-                    InlineQueryResultArticle(
-                        title="No Torrents Found in ThePirateBay!",
-                        description=f"Can't find torrents for {query} in ThePirateBay !!",
-                        input_message_content=InputTextMessageContent(
-                            message_text=f"No Torrents Found For `{query}` in ThePirateBay !!",
-                            parse_mode="Markdown"
-                        ),
-                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Try Again", switch_inline_query_current_chat="!pb ")]])
-                    )
-                )
