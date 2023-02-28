849bb2cb435ad4f124e26d655f97fd91763171c3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-01 02:38:26 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,64 @@
+from pyrogram.types import InlineKeyboardButton
+from creds import cred
+
+welcome = "\n🔥 Give me the subtitle file you want to translate.\n\n🏵️ Join & ❤️ support @PF_HW\n\n🏵️ Tv/Web series @PF_TV"
+about = (
+    "🍎 Channel : [PF HW](t.me/PF_HW)\n👨‍💻 Creater: [ ፕ๏χɪϲ ɯ๏ʅϝ ](t.me/Ab_wolf)\n⚡️ Language: ["
+    "Python3](https://python.org)\n📦 Library : [Pyrogram](https://docs.pyrogram.org/) \n💠 Server  : [Heroku]("
+    "https://herokuapp.com/) "
+)
+help_text = (
+    "**Steps to follow**\n\n`This is a subtitle translator bot`\n**1.** `Send me the subtitle file inorder to "
+    "translate.`\n**2.** `Select the destination language(dont press multiple buttons).`\n**3.** `Wait some time "
+    "to complete the translation.`\n\n**Keep in mind**\n\n**1.** `You can only translate one subtitle at a "
+    "time`\n**2.** `Dont forward bulk files together , You will be banned` "
+)
+eta_text = (
+    "**File name :** `{}`\n**Done** `{}` **of** `{}`\n**Percentage:** {}%\n**Speed:** {} lines/sec\n**ETA:** {}\n[{"
+    "}{}] "
+)
+caption = f"🥺 Please join  @PF_HW"
+empty = "🤔 You need to send a subtitle (srt) file inorder to translate it "
+mmtypes = [
+    "text/plain",
+    "application/x-subrip",
+    "application/octet-stream",
+    "application/binary",
+]
+err1 = "**__One subtitle is processing wait sometime__**"
+err2 = "**__This is not a subtitle(srt) file__**"
+err3 = "**Todays limit exceeded**"
+err4 = "**Unsupported characters in file**"
+err5 = "**Some errors happened Try again..**"
+
+langs = [
+    [
+        InlineKeyboardButton("🇮🇳 മലയാളം", callback_data="Malayalam"),
+        InlineKeyboardButton("🇮🇳 தமிழ்", callback_data="Tamil"),
+        InlineKeyboardButton("🇮🇳 हिन्दी", callback_data="Hindi"),
+    ],
+    [
+        InlineKeyboardButton("🇮🇳 ಕನ್ನಡ", callback_data="Kannada"),
+        InlineKeyboardButton("🇮🇳 తెలుగు", callback_data="Telugu"),
+        InlineKeyboardButton("🇮🇳 मराठी", callback_data="Marathi"),
+    ],
+    [
+        InlineKeyboardButton("🇮🇳 ગુજરાતી", callback_data="Gujarati"),
+        InlineKeyboardButton("🇮🇳 ଓଡ଼ିଆ", callback_data="Odia"),
+        InlineKeyboardButton("🇮🇳 বাংলা", callback_data="bn"),
+    ],
+    [
+        InlineKeyboardButton("🇮🇳 ਪੰਜਾਬੀ", callback_data="Punjabi"),
+        InlineKeyboardButton("🇮🇷 فارسی", callback_data="Persian"),
+        InlineKeyboardButton("🇺🇲 English", callback_data="English"),
+    ],
+    [
+        InlineKeyboardButton("🇪🇸 español", callback_data="Spanish"),
+        InlineKeyboardButton("🇫🇷 français", callback_data="French"),
+        InlineKeyboardButton("🇷🇺 русский", callback_data="Russian"),
+    ],
+    [
+        InlineKeyboardButton("🇮🇱 עִברִית", callback_data="hebrew"),
+        InlineKeyboardButton("🇦🇪 العربية", callback_data="arabic"),
+    ],
+]
