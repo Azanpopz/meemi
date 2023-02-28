f82372a3448cb9670d3856e4991e44f352d6f9cc
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-01 00:32:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,7 +52,7 @@ async def start_message(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2)
-                return
+        return
            buttons = [
             [
                 InlineKeyboardButton('😍😍', url="https://t.me/nasrani_batch_store")
