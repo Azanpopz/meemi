6793f31a9839f30b5f2399579c9e8605749a9282
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-28 20:44:04 +0530

    Update force.py

--- plugins/myplugins/force.py
+++ plugins/myplugins/force.py
@@ -3,6 +3,12 @@ from pyrogram import Client, filters
 
 import requests
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filter
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from pyrogram.types import CallbackQuery
+import randam
+import os
+
 
 force_channel = "+r_y-yTPhXkQwMzdl"
 
@@ -10,10 +16,10 @@ force_channel = "+r_y-yTPhXkQwMzdl"
 
 
 @Client.on_message(filters.command("star")) 
-async def start_message(client, message)
+async def start_message(bot, message)
    if  force_channel:
         try:
-            user = await client.get_chat_member(force_channel, message.from_user.id)
+            user = await bot.get_chat_member(force_channel, message.from_user.id)
             if user.status == "kicked out":
                 await message.reply_text("Join channel")
                 return
