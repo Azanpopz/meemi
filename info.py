2b2b14bd97e584b4a0ebb2cc95779b8373fff5d8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-28 15:35:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,6 +59,20 @@ class evamaria(Client):
             sleep_threshold=60
         )
 
+
+# (c) @AbirHasan2005
+
+
+
+
+class Config(object):
+    SESSION_NAME = os.environ.get("SESSION_NAME")
+    API_ID = int(os.environ.get("API_ID", 12345678))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
+
+
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
