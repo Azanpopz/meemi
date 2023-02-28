38aef2114c035b79b123be769f426df30ca27d7f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-01 00:48:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,17 +53,16 @@ async def start_message(client, message):
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2)
         return
-        buttons = [
-         [
-             InlineKeyboardButton('😍😍', url="https://t.me/nasrani_batch_store")
-         ],
-         [
-             InlineKeyboardButton('ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
-         ]
-         ]
-     reply_markup = InlineKeyboardMarkup(buttons)
-     await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
-     await asyncio.sleep(2)
+        await message.reply_text(                        
+                        text="Hello {msg.from_user.mention}   Bro സുഖമാണോ ചാനൽ ലോഗിൻ ചെയ്യ്",                                                
+                        reply_markup=InlineKeyboardMarkup(
+                            [
+                             [
+                              InlineKeyboardButton("❤️‍🔥 ᴄʜᴀɴɴᴇʟ​ ❤️‍🔥", url="https://t.me/nasrani_update")
+                             ]
+                            ]
+                        )
+                    )
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
