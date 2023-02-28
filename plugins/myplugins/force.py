abd2b41bf1a8cb9c3ff5588faa1f8cfa9df33d3d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-28 20:33:51 +0530

    Update force.py

--- plugins/myplugins/force.py
+++ plugins/myplugins/force.py
@@ -15,19 +15,19 @@ async def start_message(bot, message)
        try:
            user = await bot.get_chat_member(force_channel, msg.from_user.id)
            if user.status == "kicked out":
-       await msg.reply_text("Join channel")
-       return
-   expect UserNotParticipant:
+               await msg.reply_text("Join channel")
+               return
+       expect UserNotParticipant:
               
-      button = [[
-         InlineKeyboardButton("Mo Tech YT", url="https://t.me/+r_y-yTPhXkQwMzdl")
-         ]]             
-       await msg.reply_text(
+          button = [[
+             InlineKeyboardButton("Mo Tech YT", url="https://t.me/+r_y-yTPhXkQwMzdl")
+             ]]             
+           await msg.reply_text(
         
-       text="Hello {msg.from_user.mention}   Bro സുഖമാണോ ചാനൽ ലോഗിൻ ചെയ്യ്",
-       reply_markup=InlineKeyboardMarkup(buttons)
-   ) 
+           text="Hello {msg.from_user.mention}   Bro സുഖമാണോ ചാനൽ ലോഗിൻ ചെയ്യ്",
+           reply_markup=InlineKeyboardMarkup(buttons)
+       ) 
    
-        await msg.reply_text("done")
+           await msg.reply_text("done")
 
       
