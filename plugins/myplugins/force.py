893692833a550c2a466fb5e142ac9d6535c2173b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-28 20:35:37 +0530

    Update force.py

--- plugins/myplugins/force.py
+++ plugins/myplugins/force.py
@@ -13,21 +13,21 @@ force_channel = "+r_y-yTPhXkQwMzdl"
 async def start_message(bot, message)
    if  force_channel:
         try:
-           user = await bot.get_chat_member(force_channel, msg.from_user.id)
-           if user.status == "kicked out":
-               await msg.reply_text("Join channel")
-               return
-       expect UserNotParticipant:
+            user = await bot.get_chat_member(force_channel, msg.from_user.id)
+            if user.status == "kicked out":
+                await msg.reply_text("Join channel")
+                return
+        expect UserNotParticipant:
               
-          button = [[
-             InlineKeyboardButton("Mo Tech YT", url="https://t.me/+r_y-yTPhXkQwMzdl")
-             ]]             
-           await msg.reply_text(
+           button = [[
+              InlineKeyboardButton("Mo Tech YT", url="https://t.me/+r_y-yTPhXkQwMzdl")
+              ]]             
+            await msg.reply_text(
         
-           text="Hello {msg.from_user.mention}   Bro സുഖമാണോ ചാനൽ ലോഗിൻ ചെയ്യ്",
-           reply_markup=InlineKeyboardMarkup(buttons)
-       ) 
+            text="Hello {msg.from_user.mention}   Bro സുഖമാണോ ചാനൽ ലോഗിൻ ചെയ്യ്",
+            reply_markup=InlineKeyboardMarkup(buttons)
+        ) 
    
-           await msg.reply_text("done")
+            await msg.reply_text("done")
 
       
