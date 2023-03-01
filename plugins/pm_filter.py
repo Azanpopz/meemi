c73da2a65e6b75456a17f65363e687a024679d50
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-01 19:58:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -985,7 +985,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             user = await client.get_users(query.from_user.id)
             reply_markup = InlineKeyboardMarkup(buttons)                    
             content = query.message.text
-            await query.message.edit_text(f"{query.from_user.mention} \n<b><strike>{content}</strike></b>")
+            await query.message.edit_text(f"{query.from_user.mention} {query.from_user.id} {mention} ({reporter})\n<b><strike>{content}</strike></b>")
             await query.message.edit_reply_markup(reply_markup)
             await query.answer("Sᴇᴛ ᴛᴏ Uᴘʟᴏᴀᴅᴇᴅ !")
             try:
