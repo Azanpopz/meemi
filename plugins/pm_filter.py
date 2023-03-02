078dd9c3e424f966539c8c6d82e07666c80aabc8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-02 15:30:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -26,45 +26,6 @@ from database.users import db
 from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
 from database.ia import Media, get_file_details, get_search_results, get_bad_files
 
-
-
-
-
-
-
-
-import asyncio
-import re
-import ast
-import math
-from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from Script import script
-import pyrogram
-from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
-    make_inactive
-from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB,REQ_CHANNEL, \
-    SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ForceReply
-from pyrogram import Client, filters, enums
-from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
-from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_search_results
-from database.lazy_utils import progress_for_pyrogram, convert, humanbytes
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-import os 
-import humanize
-from PIL import Image
-import time
-from database.filters_mdb import (
-    del_all,
-    find_filter,
-    get_filters,
-)
-req_channel = REQ_CHANNEL
-
-
 from database.filters_mdb import (
     del_all,
     find_filter,
@@ -112,105 +73,6 @@ async def fil_mod(client, message):
 
 
 
-@Client.on_callback_query(filters.regex('rename'))
-async def rename(bot,update):
-	user_id = update.message.chat.id
-	date = update.message.date
-	await update.message.delete()
-	await update.message.reply_text("»»——— 𝙋𝙡𝙚𝙖𝙨𝙚 𝙚𝙣𝙩𝙚𝙧 𝙣𝙚𝙬 𝙛𝙞𝙡𝙚 𝙣𝙖𝙢𝙚...",	
-	reply_to_message_id=update.message.reply_to_message.id,  
-	reply_markup=ForceReply(True))  
-# Born to make history @LazyDeveloper !
-@Client.on_callback_query(filters.regex("upload"))
-async def doc(bot, update):
-    type = update.data.split("_")[1]
-    new_name = update.message.text
-    new_filename = new_name.split(":-")[1]
-    file = update.message.reply_to_message
-    file_path = f"downloads/{new_filename}"
-    ms = await update.message.edit("\n༻☬ད 𝘽𝙪𝙞𝙡𝙙𝙞𝙣𝙜 𝙇𝙖𝙯𝙮 𝙈𝙚𝙩𝙖𝘿𝙖𝙩𝙖...")
-    c_time = time.time()
-    try:
-        path = await bot.download_media(
-                message=file,
-                progress=progress_for_pyrogram,
-                progress_args=("**\n  ღ♡ ꜰɪʟᴇ ᴜɴᴅᴇʀ ᴄᴏɴꜱᴛʀᴜᴄᴛɪᴏɴ... ♡♪**", ms, c_time))
-    except Exception as e:
-        await ms.edit(e)
-        return 
-    splitpath = path.split("/downloads/")
-    dow_file_name = splitpath[1]
-    old_file_name =f"downloads/{dow_file_name}"
-    os.rename(old_file_name, file_path)
-    duration = 0
-    try:
-        metadata = extractMetadata(createParser(file_path))
-        if metadata.has("duration"):
-           duration = metadata.get('duration').seconds
-    except:
-        pass
-    user_id = int(update.message.chat.id) 
-    ph_path = None 
-    media = getattr(file, file.media.value)
-    filesize = humanize.naturalsize(media.file_size) 
-    c_caption = await db.get_caption(update.message.chat.id)
-    c_thumb = await db.get_thumbnail(update.message.chat.id)
-    if c_caption:
-         try:
-             caption = c_caption.format(filename=new_filename, filesize=humanize.naturalsize(media.file_size), duration=convert(duration))
-         except Exception as e:
-             await ms.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
-             return 
-    else:
-        caption = f"**{new_filename}** \n\n⚡️Data costs: `{filesize}`"
-    if (media.thumbs or c_thumb):
-        if c_thumb:
-           ph_path = await bot.download_media(c_thumb) 
-        else:
-           ph_path = await bot.download_media(media.thumbs[0].file_id)
-        Image.open(ph_path).convert("RGB").save(ph_path)
-        img = Image.open(ph_path)
-        img.resize((320, 320))
-        img.save(ph_path, "JPEG")
-    await ms.edit("三 𝘗𝘳𝘦𝘱𝘢𝘳𝘪𝘯𝘨 𝘵𝘰 𝘳𝘦𝘤𝘦𝘪𝘷𝘦 𝘓𝘢𝘻𝘺 𝘧𝘪𝘭𝘦...︻デ═一")
-    c_time = time.time() 
-    try:
-       if type == "document":
-          await bot.send_document(
-	        update.message.chat.id,
-                   document=file_path,
-                   thumb=ph_path, 
-                   caption=caption, 
-                   progress=progress_for_pyrogram,
-                   progress_args=( "**⎝⎝✧ ʀᴇᴄɪᴇᴠɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ʟᴀᴢʏ ꜱᴇʀᴠᴇʀ ✧⎠⎠**",  ms, c_time))
-       elif type == "video": 
-           await bot.send_video(
-	        update.message.chat.id,
-	        video=file_path,
-	        caption=caption,
-	        thumb=ph_path,
-	        duration=duration,
-	        progress=progress_for_pyrogram,
-	        progress_args=( "**⎝⎝✧ ʀᴇᴄɪᴇᴠɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ʟᴀᴢʏ ꜱᴇʀᴠᴇʀ ✧⎠⎠**",  ms, c_time))
-       elif type == "audio": 
-           await bot.send_audio(
-	        update.message.chat.id,
-	        audio=file_path,
-	        caption=caption,
-	        thumb=ph_path,
-	        duration=duration,
-	        progress=progress_for_pyrogram,
-	        progress_args=( "**⎝⎝✧ ʀᴇᴄɪᴇᴠɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ʟᴀᴢʏ ꜱᴇʀᴠᴇʀ ✧⎠⎠**",  ms, c_time   )) 
-    except Exception as e: 
-        await ms.edit(f" Erro {e}") 
-        os.remove(file_path)
-        if ph_path:
-          os.remove(ph_path)
-        return 
-    await ms.delete() 
-    os.remove(file_path) 
-    if ph_path:
-       os.remove(ph_path)
 
 
 
@@ -274,6 +136,22 @@ async def give_filter(client,message):
             await auto_filter(client, message)
 
 
+@Client.on_message(filters.group | filters.private & filters.text & filters.incoming)
+async def give_filter(client, message):
+    if message.chat.id != SUPPORT_CHAT_ID:
+        await global_filters(client, message)
+    manual = await manual_filters(client, message)
+    if manual == False:
+        settings = await get_settings(message.chat.id)
+        try:
+            if settings['auto_ffilter']:
+                await auto_filter(client, message)
+        except KeyError:
+            grpid = await active_connection(str(message.from_user.id))
+            await save_group_settings(grpid, 'auto_ffilter', True)
+            settings = await get_settings(message.chat.id)
+            if settings['auto_ffilter']:
+                await auto_filter(client, message) 
 
 
 # @Client.on_message(filters.private & filters.text & filters.incoming)
