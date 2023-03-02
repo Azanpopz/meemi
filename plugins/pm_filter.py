4090adeb16ebaf7d9ef55c028f02c9445a5440af
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-02 23:47:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1200,12 +1200,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
          ],[
             InlineKeyboardButton('ʙᴀᴄᴋ ᴛᴏ sᴛᴀʀᴛ', callback_data='surprise')
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await client.edit_message_media(
-            query.message.chat.id, 
-            query.message.id, 
-            
-        )
+        reply_markup = InlineKeyboardMarkup(buttons)        
+        
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
