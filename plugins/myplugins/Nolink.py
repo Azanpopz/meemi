14d9228f35329f3fa7084ba251c2adaa49318985
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-02 19:16:16 +0530

    Update Nolink.py

--- plugins/myplugins/Nolink.py
+++ plugins/myplugins/Nolink.py
@@ -8,6 +8,38 @@ from Script import script
 from info import PICS, REQST_CHANNEL, SUPPORT_CHAT_ID
 import random
 
+
+import imp
+import os
+import logging
+import random
+import asyncio
+from Script import script
+from pyrogram import Client, filters, enums
+from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.ia import Media, get_file_details, unpack_new_file_id
+from database.users_chats_db import db
+from database.users import db
+
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, MSG_ALRT, MAIN_CHANNEL, MY_CHANNEL
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT_ID, MAX_B_TN, VERIFY, MVG_LNK, OWN_LNK
+
+from util import get_settings, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token
+
+from plugins.fsub import ForceSub
+
+from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
+from database.connections_mdb import active_connection
+import re
+import json
+import base64
+logger = logging.getLogger(__name__)
+
+
+
+
+
 Bot = Client(
     "NoLink-BOT",
     bot_token = os.environ["BOT_TOKEN"],
@@ -21,8 +53,7 @@ async def nolink(bot,message):
     if message.reply_to_message and SUPPORT_CHAT_ID == message.chat.id:
         chat_id = message.chat.id
         reporter = str(message.from_user.id)
-        mention = message.from_user.mention
-        success = True
+        mention = message.from_user.mention        
         content = message.reply_to_message.text
         try:
             if REQST_CHANNEL is not None:
@@ -31,17 +62,7 @@ async def nolink(bot,message):
                         InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
                       ]]
                 reported_post = await bot.send_message(chat_id=REQST_CHANNEL, text=f"🤯𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n\n 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
-                success = True
-            elif len(content) >= 3:
-                for admin in ADMINS:
-                    btn = [[
-                        InlineKeyboardButton('View Request', url=f"{message.reply_to_message.link}"),
-                        InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                      ]]
-                    reported_post = await bot.send_message(chat_id=admin, text=f"🙂𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n \n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
-                    success = True
-            else:
-                if len(content) < 3:
+                
                     await message.delete() 
                 
                 return
