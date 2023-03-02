f18b2b4613f9afb4d1e753dc7e5f08c654e3efde
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-02 16:43:14 +0530

    Update Nolink.py

--- plugins/myplugins/Nolink.py
+++ plugins/myplugins/Nolink.py
@@ -20,18 +20,18 @@ async def nolink(bot,message):
                 hmm = await message.delete()
                 return
                 buttons = [[
-            InlineKeyboardButton('sᴜʀᴘʀɪsᴇ', callback_data='start')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAINdmL9uWnC3ptj9YnTjFU4YGr5dtzwAAIEAAPBJDExieUdbguzyBAeBA") 
-        await asyncio.sleep(1)
-        await m.delete()        
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
+                    InlineKeyboardButton('sᴜʀᴘʀɪsᴇ', callback_data='start')
+                ]]
+                reply_markup = InlineKeyboardMarkup(buttons)
+                m=await message.reply_sticker("CAACAgUAAxkBAAINdmL9uWnC3ptj9YnTjFU4YGr5dtzwAAIEAAPBJDExieUdbguzyBAeBA") 
+                await asyncio.sleep(1)
+                await m.delete()        
+                await message.reply_photo(
+                    photo=random.choice(PICS),
+                    caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+                    reply_markup=reply_markup,
+                    parse_mode=enums.ParseMode.HTML
+                )
 
 
 	except:
