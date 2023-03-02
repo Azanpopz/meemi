243add05186c86d18117c9205c7f752d454777c5
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-02 23:38:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,9 +60,8 @@ async def start(client, message):
         m=await message.reply_sticker("CAACAgUAAxkBAAINdmL9uWnC3ptj9YnTjFU4YGr5dtzwAAIEAAPBJDExieUdbguzyBAeBA") 
         await asyncio.sleep(1)
         await m.delete()        
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(            
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
