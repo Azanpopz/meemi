a6b346810d32aeef3be20ae149d7b42e6af9083e
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-02 14:46:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -27,6 +27,29 @@ from database.ia_filterdb import Media, get_file_details, get_search_results, ge
 from database.ia import Media, get_file_details, get_search_results, get_bad_files
 
 
+
+
+
+
+
+
+import asyncio
+import re
+import ast
+import math
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from Script import script
+import pyrogram
+from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
+    make_inactive
+from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB,REQ_CHANNEL, \
+    SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ForceReply
+from pyrogram import Client, filters, enums
+from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
+from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
+from database.users_chats_db import db
+from database.ia_filterdb import Media, get_file_details, get_search_results
 from database.lazy_utils import progress_for_pyrogram, convert, humanbytes
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
@@ -34,6 +57,13 @@ import os
 import humanize
 from PIL import Image
 import time
+from database.filters_mdb import (
+    del_all,
+    find_filter,
+    get_filters,
+)
+req_channel = REQ_CHANNEL
+
 
 from database.filters_mdb import (
     del_all,
