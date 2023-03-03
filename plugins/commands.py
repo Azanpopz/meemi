cf4c4678ca83a2a98cf225e3fc9202274b125314
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-03 18:43:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -206,9 +206,9 @@ async def start(client, message):
                                  )
                              )
                 
-                await client.send_cached_media(
+                await message.reply_text(
                     chat_id=BATCH_GROUP,
-                    caption=script.BATCH_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),)
+                    text=script.BATCH_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),)
                     
                 
                 
