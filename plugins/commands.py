eea7411ab4f64fc98b4c26d3d81cebdc79783043
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-03 07:07:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -209,7 +209,7 @@ async def start(client, message):
                 await asyncio.sleep(e.x)
                 logger.warning(f"Floodwait of {e.x} sec.")
                 
-                k = await client.send_cached_media(
+                await client.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,      
@@ -228,8 +228,7 @@ async def start(client, message):
                                  )
                              )
 
-                await asyncio.sleep(60) 
-                await k.delete()
+                
                 await message.reply(f"<b><a href='https://t.me/NasraniChatGroup'>Thank For Using Me...</a></b>")
      
         
