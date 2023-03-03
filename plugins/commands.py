b515b7c567c32b79185ae36322d04048f82759ac
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-03 18:18:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -207,7 +207,7 @@ async def start(client, message):
                              )
                 
                 await message.reply(
-                    chat_id=force_channel,
+                    chat_id=BATCH_GROUP,
                     text=script.BATCH_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
                     
                 )
