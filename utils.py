990a39989364f4f6ee9d8f4fd39195c2973a0e71
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-05 23:26:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ import os
 from datetime import datetime
 from typing import List
 from database.users_chats_db import db
-from database.users import db
+
 
 from bs4 import BeautifulSoup
 import requests
