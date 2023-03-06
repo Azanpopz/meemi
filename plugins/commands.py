1cb9d5e371992124c1de6a8b08d3c26db04c7c94
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-06 13:29:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,8 +16,8 @@ from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_C
 
 
 from plugins.fsub import ForceSub
+from utils import get_settings, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token
 
-from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
 import re
 import json
