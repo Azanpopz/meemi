7bff54f78d102395448d490f563095b2c47866eb
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-07 02:18:03 +0530

    Update caption.py

--- plugins/myplugins/caption.py
+++ plugins/myplugins/caption.py
@@ -1,63 +1,45 @@
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K & PR0FESS0R-99
 
-import asyncio
-from pyrogram import Client, filters
-from bot import Bot
+import os
 from config import Config
+from pyrogram import Client, filters 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import FloodWait
+
+CAPTION_TEXT=Config.CAPTION
+BUTTON_TEXT=Config.BUTTON_TEXT
+URL_LINK=Config.URL_LINK
 
 
-# =
-usercaption_position = Config.CAPTION_POSITION
-caption_position = usercaption_position.lower()
-caption_text = Config.CAPTION_TEXT
+@Client.on_message(filters.media & filters.channel)
+async def caption(client, message: Message):
+    kopp, _ = get_file_id(message)
+    await message.edit(f"<b>{kopp.file_name}</b>\n\n{CAPTION_TEXT}",
+          reply_markup=InlineKeyboardMarkup(
+              [[
+              InlineKeyboardButton(f"{BUTTON_TEXT}", url=f"{URL_LINK}")
+              ]]
+        ))
 
-@Client.on_message(filters.channel & (filters.document | filters.video | filters.audio) & ~filters.edited, group=-1)
-async def editing(bot, message):
-      try:
-         media = message.document or message.video or message.audio or message.image
-         caption_text = Config.CAPTION_TEXT
-      except:
-         caption_text = ""
-         pass 
-      if (message.document or message.video or message.audio or message.image): 
-          if message.caption:
-             fname = media.file_name
-             filename = fname.replace("_", ".")
-             file_caption = f"`{filename}`"                
-          else:
-             fname = media.file_name
-             filename = fname.replace("_", ".")
-             file_caption = f"`{filename}`"  
-              
-      try:
-          if caption_position == "top":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id, 
-                 message_id = message.message_id,
-                 caption = caption_text + "\n" + file_caption,
-                 parse_mode = "markdown"
-             )
-          elif caption_position == "bottom":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id, 
-                 message_id = message.message_id,
-                 caption = file_caption + "\n" + caption_text,
-                 parse_mode = "markdown"
-             )
-          elif caption_position == "nil":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id,
-                 message_id = message.message_id,
-                 caption = caption_text, 
-                 parse_mode = "markdown"
-             ) 
-      except:
-          pass
-
-@Client.on_message(filters.command("hello"))
-async def hello(bot, message):
-  await bot.reply("hello")
-      
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker"
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                return obj, obj.file_id
