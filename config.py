7d0bd0a2bcafefdeb4b6124fa5a3a48a2a8bad93
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-07 02:20:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
+# <C> MoTechYT
+
+
 import os
 
-
-
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "💕💕")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "top")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "kinzanoufal")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
+    CAPTION = os.environ.get("CAPTION", "💕💕")
+    BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
+    URL_LINK = os.environ.get("LINK", "T.ME/nasrani_update")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@KOCHU_KALLAN_RoBOT")
