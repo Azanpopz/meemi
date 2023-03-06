0664b9309e8f04b1c623b4e95eb3b4194ad10b43
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-06 12:25:51 +0530

    Update Delete.py

--- plugins/myplugins/Delete.py
+++ plugins/myplugins/Delete.py
@@ -11,7 +11,7 @@ API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
 SESSION = environ.get('SESSION', 'UFSBotz')
-TIME = int(environ.get("TIME"))
+
 GROUPS = []
 for grp in environ.get("GROUPS").split():
     GROUPS.append(int(grp))
