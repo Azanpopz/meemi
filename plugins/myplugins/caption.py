49fba316f0a67995caa5d40451f2ebc457133460
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-07 01:49:01 +0530

    Update caption.py

--- plugins/myplugins/caption.py
+++ plugins/myplugins/caption.py
@@ -23,7 +23,7 @@ class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "😍😍")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "top")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "AvishkarPatil")
 
