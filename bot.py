bcfce8cdef251d65accba65af9d651d2e360cb58
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-07 02:16:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,17 +22,6 @@ import pytz
 
 
 
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-
-from config import Config
-from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 
 
