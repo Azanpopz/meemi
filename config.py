eb0adec107e591f81daf51a56b339c58c9ee45be
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-07 02:30:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "2034622427:AAGzgpj4Viycp2rjB5TwgYdaYzfP3ImXSoA")
+    API_ID = int(os.environ.get("API_ID", 1778836))
+    API_HASH = os.environ.get("API_HASH", "7bcf61fcd32b8652cd5876b02dcf57ae")
     CAPTION = os.environ.get("CAPTION", "💕💕")
     BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
     URL_LINK = os.environ.get("LINK", "T.ME/nasrani_update")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@KOCHU_KALLAN_RoBOT")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "KOCHU_KALLAN_RoBOT")
