0fc200c7389daa87e82c3899224a90f0287c2826
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-07 02:10:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,40 @@ from Script import script
 from datetime import date, datetime 
 import pytz
 
+
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+from config import Config
+from pyrogram import Client 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+class autocaption(Client):
+    
+    def __init__(self):
+        super().__init__(
+            session_name="Captioner",
+            bot_token = Config.BOT_TOKEN,
+            api_id = Config.API_ID,
+            api_hash = Config.API_HASH,
+            workers = 20,
+            plugins = dict(
+                root="Plugins"
+            )
+        )
+
+if __name__ == "__main__" :
+    
+
+
+
+
+
 class Bot(Client):
 
     def __init__(self):
