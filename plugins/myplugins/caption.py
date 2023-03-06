550da66318aa55e5f29c41abb17650b51e76207a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-07 01:48:22 +0530

    Create caption.py

---
+++ plugins/myplugins/caption.py
@@ -0,0 +1,98 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import asyncio
+from pyrogram import Client, filters
+
+import pyrogram
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+from config import Config
+from pyrogram import Client 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+class Config(object):
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+      API_ID = int(os.environ.get("APP_ID", 12345))
+      API_HASH = os.environ.get("API_HASH")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "top")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "AvishkarPatil")
+
+
+class autocaption(Client):
+    
+    def __init__(self):
+        super().__init__(
+            session_name="Captioner",
+            bot_token = Config.BOT_TOKEN,
+            api_id = Config.API_ID,
+            api_hash = Config.API_HASH,
+            workers = 20,
+            plugins = dict(
+                root="Plugins"
+            )
+        )
+
+if __name__ == "__main__" :
+
+
+
+
+# =
+usercaption_position = Config.CAPTION_POSITION
+caption_position = usercaption_position.lower()
+caption_text = Config.CAPTION_TEXT
+
+
+@Client.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
+async def editing(bot, message):
+      try:
+         media = message.document or message.video or message.audio
+         caption_text = Config.CAPTION_TEXT
+      except:
+         caption_text = ""
+         pass 
+      if (message.document or message.video or message.audio): 
+          if message.caption:                        
+             file_caption = f"**{message.caption}**"                
+          else:
+             fname = media.file_name
+             filename = fname.replace("_", ".")
+             file_caption = f"`{filename}`"  
+              
+      try:
+          if caption_position == "top":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.message_id,
+                 caption = caption_text + "\n" + file_caption,
+                 parse_mode = "markdown"
+             )
+          elif caption_position == "bottom":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.message_id,
+                 caption = file_caption + "\n" + caption_text,
+                 parse_mode = "markdown"
+             )
+          elif caption_position == "nil":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id,
+                 message_id = message.message_id,
+                 caption = caption_text, 
+                 parse_mode = "markdown"
+             ) 
+      except:
+          pass
+              
+                   
+      
