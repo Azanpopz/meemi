958aec31ea741db887b57833c2825223e0eb37d4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-06 13:10:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -875,7 +875,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                         )
                     )
                     if settings['auto_delete']:
-                        await asyncio.sleep(600)
+                        await asyncio.sleep(300)
                         await Joel_tgx.delete()
                         await file_send.delete()
                 else:
