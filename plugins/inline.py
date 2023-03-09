ed3e5d7d04863424a9f4666f49fa81f7b379e9d3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-09 13:52:10 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -5,6 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from database.connections_mdb import active_connection
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
