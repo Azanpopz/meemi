632cf5cbe932129bc5d983afc14956f8b0d0bfef
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 23:44:19 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -6,10 +6,14 @@ from info import BATCH_GROUP
 @Client.on_message(filters.text & filters.chat(BATCH_GROUP))
 async def song(client, message):
      
-    try:                     
+    
+       
+    
+    try:
+        pak = await message.reply('Downloading...')
         r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
     except Exception as e:
-                
+        await pak.edit(str(e))
         return
     sname = r['data']['results'][0]['name']
     slink = r['data']['results'][0]['downloadUrl'][4]['link']
@@ -20,8 +24,8 @@ async def song(client, message):
     file = wget.download(slink)
     ffile = file.replace("mp4", "mp3")
     os.rename(file, ffile)
-    
+    await pak.edit('Uploading...')
     await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
     os.remove(ffile)
     os.remove(thumbnail)
-    
+    await pak.delete()
