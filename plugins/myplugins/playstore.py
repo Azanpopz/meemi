8bb961b47edc4c31448b8888f374c72153868983
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 01:48:23 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -21,20 +21,20 @@ async def search(client, message):
   #   for result in results:
      results = play_scraper.search
        
-        details = "**Title:** `{}`".format(result["title"]) + "\n" \
-        "**Description:** `{}`".format(result["description"]) + "\n" \
-        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
-        "**Developer:** `{}`".format(result["developer"]) + "\n" \
-        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
-        "**Score:** `{}`".format(result["score"]) + "\n" \
-        "**Price:** `{}`".format(result["price"]) + "\n" \
-        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
-        "**Free:** `{}`".format(result["free"]) + "\n" \
-        "\n" + "Made by @FayasNoushad"
-        try:
-            await message.reply_text(title=result["title"], description=result.get("description", None), thumb_url=result.get("icon", None),)         
-        except Exception as error:
-            print(error)
+     details = "**Title:** `{}`".format(result["title"]) + "\n" \
+     "**Description:** `{}`".format(result["description"]) + "\n" \
+     "**App ID:** `{}`".format(result["app_id"]) + "\n" \
+     "**Developer:** `{}`".format(result["developer"]) + "\n" \
+     "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
+     "**Score:** `{}`".format(result["score"]) + "\n" \
+     "**Price:** `{}`".format(result["price"]) + "\n" \
+     "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
+     "**Free:** `{}`".format(result["free"]) + "\n" \
+     "\n" + "Made by @FayasNoushad"
+     try:
+         await message.reply_text(title=result["title"], description=result.get("description", None), thumb_url=result.get("icon", None),)         
+     except Exception as error:
+         print(error)
      
             
 
