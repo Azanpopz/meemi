987d221a8ede2c3f34abb376ba3a7eff991d44e8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 01:42:06 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -16,7 +16,7 @@ Bot = Client(
 async def search(client, message):
     
      
-     results = play_scraper.search(f"https://play.google.com/search?query={answers}&page=1&limit=1")
+     results = play_scraper.search(f"https://play.google.com/search?query={answer}&page=1&limit=1")
      answers = []
      for result in results:
  #   results = play_scraper.search(update)
