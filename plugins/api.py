5b2460266792ee6f172d3d1bed6a66d37418204e
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 20:31:06 +0530

    Update api.py

--- plugins/api.py
+++ plugins/api.py
@@ -77,28 +77,3 @@ if genius_api:
 #   aiohttpsession = ClientSession()
 #    arq = None
     
-class Mbot(Client):
-    def  __init__(self):
-        name = self.__class__.__name__.lower()
-        super().__init__(
-            ":memory:",
-            plugins=dict(root=f"{name}/plugins"),
-            workdir="./cache/",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            sleep_threshold=30
-        )
-    async def start(self):
-        os.system(f"rm -rf ./cache/")
-        os.system(f"mkdir ./cache/")
-        global BOT_INFO
-        
-        BOT_INFO = await self.get_me()
-        if not path.exists('/tmp/thumbnails/'):
-            mkdir('/tmp/thumbnails/')
-        for chat in AUTH_CHATS:
-            await self.send_photo(chat,"https://telegra.ph/file/97bc8a091ac1b119b72e4.jpg","**Spotify Downloa Started**")
-        LOGGER.info(f"Bot Started As {BOT_INFO.username}\n")
-    
-    
