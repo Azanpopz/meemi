5df9319d3fb0402b56ab7a3dbad84c3c87a403f9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 00:04:36 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -12,7 +12,7 @@ async def song(client, message):
     except Exception as e:
             await pak.edit(str(e))
             return
-        sname = r['data']['results'][0]['name']
+            sname = r['data']['results'][0]['name']
         slink = r['data']['results'][0]['downloadUrl'][4]['link']
         ssingers = r['data']['results'][0]['primaryArtists']
      #  album_id = r.json()[0]["albumid"]
