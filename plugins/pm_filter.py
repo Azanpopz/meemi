8ab7b96776e8bd466b6616a4310338106cd49113
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 01:01:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -134,7 +134,7 @@ async def give_filter(client,message):
             await auto_filter(client, message)
 
 
-@Client.on_message(filters.group | filters.private & filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     if message.chat.id != SUPPORT_CHAT_ID:
         await global_filters(client, message)
