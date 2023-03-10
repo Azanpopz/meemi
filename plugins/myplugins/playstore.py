c9145a73bf6e046470e6b3f9e3ecd70d32367c51
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 01:46:41 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -20,8 +20,7 @@ async def search(client, message):
  #    answers = []
   #   for result in results:
      results = play_scraper.search
-     answers = []
-     for result in results:   
+       
         details = "**Title:** `{}`".format(result["title"]) + "\n" \
         "**Description:** `{}`".format(result["description"]) + "\n" \
         "**App ID:** `{}`".format(result["app_id"]) + "\n" \
@@ -36,6 +35,6 @@ async def search(client, message):
             await message.reply_text(title=result["title"], description=result.get("description", None), thumb_url=result.get("icon", None),)         
         except Exception as error:
             print(error)
-     await update.answer(answers)
+     
             
 
