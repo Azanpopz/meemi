513883ef306a86914be3fe3387d8cc96b9e28bcb
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 23:54:16 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -5,14 +5,14 @@ from info import BATCH_GROUP
 
 @Client.on_message(filters.text & filters.chat(BATCH_GROUP))
 async def song(client, message):
-     
+   if BATCH_GROUP:  
     
        
     
-    try:
+        try:
        
         
-        if BATCH_GROUP:
+        
             pak = await message.reply('Downloading...')
         try
             r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
