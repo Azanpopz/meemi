cd2f2885d1ba1d3dd7109e3b92641027b43414f7
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 23:25:46 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -9,8 +9,7 @@ async def song(client, message):
        
         
         
-        pak = await message.reply('Downloading...')
-    try:
+        
         r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
     except Exception as e:
         await pak.edit(str(e))
