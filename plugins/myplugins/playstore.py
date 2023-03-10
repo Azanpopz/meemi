b107ce8dd3261a3090d35c89f908e3882006e679
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 02:04:21 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -15,21 +15,21 @@ Bot = Client(
 @Client.on_message(filters.command('app') & filters.text)
 async def search(client, message):
     
+    
+    results = play_scraper.search(f"https://play.google.com"+result["url"])]]
+    answers = []
+        for result in results:
      
-      results = play_scraper.search(f"https://play.google.com"+result["url"])]]
-      answers = []
-      for result in results:
-     
-     details = "**Title:** `{}`".format(result["title"]) + "\n" \
-     "**Description:** `{}`".format(result["description"]) + "\n" \
-     "**App ID:** `{}`".format(result["app_id"]) + "\n" \
-     "**Developer:** `{}`".format(result["developer"]) + "\n" \
-     "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
-     "**Score:** `{}`".format(result["score"]) + "\n" \
-     "**Price:** `{}`".format(result["price"]) + "\n" \
-     "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
-     "**Free:** `{}`".format(result["free"]) + "\n" \
-     "\n" + "Made by @FayasNoushad"
+        details = "**Title:** `{}`".format(result["title"]) + "\n" \
+        "**Description:** `{}`".format(result["description"]) + "\n" \
+        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
+        "**Developer:** `{}`".format(result["developer"]) + "\n" \
+        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
+        "**Score:** `{}`".format(result["score"]) + "\n" \
+        "**Price:** `{}`".format(result["price"]) + "\n" \
+        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
+        "**Free:** `{}`".format(result["free"]) + "\n" \
+        "\n" + "Made by @FayasNoushad"
      try:
          await message.reply_text(title=result["title"], description=result.get("description", None), thumb_url=result.get("icon", None),)         
      except Exception as error:
