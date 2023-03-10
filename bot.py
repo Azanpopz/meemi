d1923655a64cd0430fa2ca9edcfab3124658e28e
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 19:39:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,51 +23,6 @@ import pytz
 
 from config import Config
 
-from pyrogram import Client
-from os import environ,sys,mkdir,path
-import logging
-from sys import executable
-#from Python_ARQ import ARQ
-from aiohttp import ClientSession
-from dotenv import load_dotenv
-import shutil
-load_dotenv("config.env")
-import os 
-# Log
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(message)s",
-    handlers = [logging.FileHandler('bot.log'), logging.StreamHandler()]
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-LOGGER = logging.getLogger(__name__)
-
-# Mandatory Variable
-try:
-    API_ID = int(environ['API_ID'])
-    API_HASH = environ['API_HASH']
-    BOT_TOKEN = environ['BOT_TOKEN']
-    OWNER_ID = int(environ['OWNER_ID'])
-except KeyError:
-    LOGGER.debug("One or More ENV variable not found.")
-    sys.exit(1)
-# Optional Variable
-SUDO_USERS = environ.get("SUDO_USERS",str(OWNER_ID)).split()
-SUDO_USERS = [int(_x) for _x in SUDO_USERS]
-if OWNER_ID not in SUDO_USERS:
-    SUDO_USERS.append(OWNER_ID)
-AUTH_CHATS = environ.get('AUTH_CHATS',None ).split()
-AUTH_CHATS = [int(_x) for _x in AUTH_CHATS]
-LOG_GROUP = environ.get("LOG_GROUP", None)
-if LOG_GROUP:
-    LOG_GROUP = int(LOG_GROUP)
-BUG = environ.get("BUG", None)
-if BUG:
-    BUG = int(BUG)
-genius_api = environ.get("genius_api",None)
-if genius_api:
-    genius_api = genius_api
-
-
 
 
 class Bot(Client):
