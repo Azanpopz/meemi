6baca68862413ad8fb71ba1ee1934b759e864218
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 20:32:20 +0530

    Update youtube.py

--- plugins/myplugins/youtube.py
+++ plugins/myplugins/youtube.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
-from plugins import Mbot,genius_api
+from plugins import genius_api
 import requests 
 from lyricsgenius import Genius 
 import os
-
+from bot import Bot
 
 API = "https://apis.xditya.me/lyrics?song="
 
