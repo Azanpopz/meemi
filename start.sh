1ef7e484f77e2979c0a0f2301e271dcb19de98fe
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 13:57:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/hagadmansa.git /hagadmansa
+  git clone https://github.com/Azanpopz/meemi.git /meemi
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /hagadmansa
+  git clone $UPSTREAM_REPO /meemi
 fi
-cd /hagadmansa
+cd /meemi
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
