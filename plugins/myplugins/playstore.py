0d520c8042c668e2b7ffd29799f50d29c079ec00
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 00:50:54 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -2,7 +2,7 @@ import os
 import play_scraper
 from pyrogram import Client, filters
 from pyrogram.types import *
-
+from info import BATCH_GROUP
 
 Bot = Client(
     "Play-Store-Bot",
@@ -12,27 +12,9 @@ Bot = Client(
 )
 
 
-@Bot.on_message(filters.private & filters.all)
-async def filter_all(bot, update):
-    text = "Search play store apps using below buttons.\n\nMade by @FayasNoushad"
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
-            [InlineKeyboardButton(text="Search in another chat", switch_inline_query="")]
-        ]
-    )
-    await update.reply_text(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
-    )
-
-
-@Bot.on_inline_query()
-async def search(bot, update):
-    results = play_scraper.search(update.query)
-    answers = []
+@Client.on_message(filters.command('app')filters.text & filters.chat(BATCH_GROUP))
+async def search(client, message):
+    
     for result in results:
         details = "**Title:** `{}`".format(result["title"]) + "\n" \
         "**Description:** `{}`".format(result["description"]) + "\n" \
@@ -44,24 +26,9 @@ async def search(bot, update):
         "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
         "**Free:** `{}`".format(result["free"]) + "\n" \
         "\n" + "Made by @FayasNoushad"
-        reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
-        )
-        try:
-            answers.append(
-                InlineQueryResultArticle(
-                    title=result["title"],
-                    description=result.get("description", None),
-                    thumb_url=result.get("icon", None),
-                    input_message_content=InputTextMessageContent(
-                        message_text=details, disable_web_page_preview=True
-                    ),
-                    reply_markup=reply_markup
-                )
-            )
+        await message.reply_text(title=result["title"], description=result.get("description", None), thumb_url=result.get("icon", None),)         
+            
         except Exception as error:
             print(error)
     await update.answer(answers)
 
-
-Bot.run()
