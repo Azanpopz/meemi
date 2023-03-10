567acfcd850474d04102290e13930becd9143ebe
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 00:05:22 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -13,16 +13,16 @@ async def song(client, message):
             await pak.edit(str(e))
             return
             sname = r['data']['results'][0]['name']
-        slink = r['data']['results'][0]['downloadUrl'][4]['link']
-        ssingers = r['data']['results'][0]['primaryArtists']
-     #  album_id = r.json()[0]["albumid"]
-        img = r['data']['results'][0]['image'][2]['link']
-        thumbnail = wget.download(img)
-        file = wget.download(slink)
-        ffile = file.replace("mp4", "mp3")
-        os.rename(file, ffile)
-        await pak.edit('Uploading...')
-        await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
-        os.remove(ffile)
-        os.remove(thumbnail)
-        await pak.delete()
+            slink = r['data']['results'][0]['downloadUrl'][4]['link']
+            ssingers = r['data']['results'][0]['primaryArtists']
+     #      album_id = r.json()[0]["albumid"]
+            img = r['data']['results'][0]['image'][2]['link']
+            thumbnail = wget.download(img)
+            file = wget.download(slink)
+            ffile = file.replace("mp4", "mp3")
+            os.rename(file, ffile)
+            await pak.edit('Uploading...')
+            await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
+            os.remove(ffile)
+            os.remove(thumbnail)
+            await pak.delete()
