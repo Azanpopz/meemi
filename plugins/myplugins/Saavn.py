e2a5be377df8540b9f69ad2ffe111a016127f0c7
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-10 23:41:24 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -5,14 +5,11 @@ from info import BATCH_GROUP
 
 @Client.on_message(filters.text & filters.chat(BATCH_GROUP))
 async def song(client, message):
-    try:
-       args = message.text.split(None, 1)[1]
-    except:               
-        pak = await message.reply('saavn requires an argument.') 
+     
     try:                     
         r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
     except Exception as e:
-        await pak.edit(str(e))        
+                
         return
     sname = r['data']['results'][0]['name']
     slink = r['data']['results'][0]['downloadUrl'][4]['link']
@@ -23,7 +20,7 @@ async def song(client, message):
     file = wget.download(slink)
     ffile = file.replace("mp4", "mp3")
     os.rename(file, ffile)
-    await pak.edit('Uploading...')
+    
     await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
     os.remove(ffile)
     os.remove(thumbnail)
