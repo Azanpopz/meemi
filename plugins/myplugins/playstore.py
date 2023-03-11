7376ad655b36971fcad44d3eaa06318a0e3a1419
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 13:24:11 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -18,7 +18,8 @@ Bot = Client(
 
 
 @Client.on_message(filters.private & filters.command(["google"]))
-async def start(bot, update):    
+async def start(bot, update):
+    args = message.text.split(None)  
     r = requests.get(API + requote_uri(f"https://api.abirhasan.wtf/google?query={args}&page=1&limit=10").json()
     informations = r.json()["results"][:50]
     text = f"**Title:** `{info['title']}`"
