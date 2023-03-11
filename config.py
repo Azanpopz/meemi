0650b77520633e4a877bc2985a36637a464c3786
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-12 02:19:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
 
+import os
+
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
+    DB_URL = os.environ.get("DATABASE_URL", "")
+
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("APP_ID", 12345))
