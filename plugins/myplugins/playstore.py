31981c037737594bec2fcdc75db49f87f2c7e431
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 16:31:35 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -225,7 +225,7 @@ MINNAL_MURALI = InlineKeyboardMarkup(
 
 
 
-@Client.on_message(filters.command(["sticker", "s"]) & filters.reply & filters.group)
+@Client.on_message(filters.command(["sq", "qs"]) & filters.reply & filters.group)
 async def create_sticker_group_handler(c: Client, m: Message):
     s = await m.reply_text("...", reply_to_message_id=m.message_id)
     await create_sticker(c, m.reply_to_message)
