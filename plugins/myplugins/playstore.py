95e1c65b5e9d70ab86b9b6ccc4ced27c23551688
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 12:31:03 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -10,7 +10,12 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 
 
-
+Bot = Client(
+    "Play-Store-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
 
 
 @Client.on_inline_query()
