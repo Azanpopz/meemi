a6f28108ada9e98b4e456d62ab016a88f91eee3c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 16:58:29 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -44,34 +44,34 @@ async def song(client, message):
 # Spotify-Downloader
 
 
-@Client.on_message(filters.command('saavn') & filters.text)
-async def song(client, message):
-    try:
-       args = message.text.split(None, 1)[1]
-    except:
-        return await message.reply("/saavn requires an argument.")
-    if args.startswith(" "):
-        await message.reply("/saavn requires an argument.")
-        return ""
-    pak = await message.reply('Downloading...')
-    try:
-        r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
-    except Exception as e:
-        await pak.edit(str(e))
-        return
-    sname = r['data']['results'][0]['name']
-    slink = r['data']['results'][0]['downloadUrl'][4]['link']
-    ssingers = r['data']['results'][0]['primaryArtists']
+#@Client.on_message(filters.command('saavn') & filters.text)
+#async def song(client, message):
+#    try:
+#       args = message.text.split(None, 1)[1]
+#    except:
+#        return await message.reply("/saavn requires an argument.")
+#    if args.startswith(" "):
+#        await message.reply("/saavn requires an argument.")
+#        return ""
+#    pak = await message.reply('Downloading...')
+#    try:
+#        r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
+#    except Exception as e:
+#        await pak.edit(str(e))
+#        return
+#    sname = r['data']['results'][0]['name']
+#    slink = r['data']['results'][0]['downloadUrl'][4]['link']
+#    ssingers = r['data']['results'][0]['primaryArtists']
   #  album_id = r.json()[0]["albumid"]
-    img = r['data']['results'][0]['image'][2]['link']
-    thumbnail = wget.download(img)
-    file = wget.download(slink)
-    ffile = file.replace("mp4", "mp3")
-    os.rename(file, ffile)
-    await pak.edit('Uploading...')
-    k = await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
-    os.remove(ffile)
-    os.remove(thumbnail)
-    await asyncio.sleep(180)
-    await k.delete()
-    await pak.delete()
+#    img = r['data']['results'][0]['image'][2]['link']
+#    thumbnail = wget.download(img)
+#    file = wget.download(slink)
+#    ffile = file.replace("mp4", "mp3")
+#    os.rename(file, ffile)
+#    await pak.edit('Uploading...')
+#    k = await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
+#    os.remove(ffile)
+#    os.remove(thumbnail)
+#    await asyncio.sleep(180)
+#    await k.delete()
+#    await pak.delete()
