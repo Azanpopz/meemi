a68bee2b5d6366a72b1fe13bf7fb556d1503875d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 13:58:15 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -1,49 +1,49 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
 import asyncio
-import random
-from asyncio import sleep
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+from config import CMD_HANDLER as cmd
+from ProjectMan.helpers.tools import get_arg
 
 
-@Client.on_message(filters.command(["q"]))
-async def quotly(client, message):
-    if not message.reply_to_message:
-        await message.edit("Reply to any users text message")
-        return
 
-    await message.edit("```Making a Quote```")
 
-    await message.reply_to_message.forward("@QuotLyBot")
-
-    is_sticker = False
-    progress = 0
-
-    while not is_sticker:
-        try:
-            await sleep(4)
-            msg = await bot.get_history("@QuotLyBot", 1)
-            print(msg)
-            is_sticker = True
-        except:
-            await sleep(1)
-
-            progress += random.randint(0, 5)
-
-            if progress > 100:
-                await message.edit('There was a long running error')
-                return
-
-            try:
-                await message.edit("```Making a Quote\nProcessing {}%```".format(progress))
-            except:
-                await message.edit("ERROR")
-
-    if msg_id := msg[0]['message_id']:
-        await asyncio.gather(
-            message.delete(),
-            bot.forward_messages(message.chat.id, "@QuotLyBot", msg_id)
-        )
+@Client.on_message(filters.me & filters.command(["q", "quotly"]))
+async def quotly(client: Client, message: Message):
+    args = get_arg(message)
+    if not message.reply_to_message and not args:
+        return await message.edit("**Mohon Balas ke Pesan**")
+    bot = "QuotLyBot"
+    if message.reply_to_message:
+        await message.edit("`Making a Quote . . .`")
+        await client.unblock_user(bot)
+        if args:
+            await client.send_message(bot, f"/qcolor {args}")
+            await asyncio.sleep(1)
+        else:
+            pass
+        await message.reply_to_message.forward(bot)
+        await asyncio.sleep(5)
+        async for quotly in client.search_messages(bot, limit=1):
+            if quotly:
+                await message.delete()
+                await message.reply_sticker(
+                    sticker=quotly.sticker.file_id,
+                    reply_to_message_id=message.reply_to_message.id
+                    if message.reply_to_message
+                    else None,
+                )
+            else:
+                return await message.edit("**Gagal Membuat Sticker Quotly**")
 
 
