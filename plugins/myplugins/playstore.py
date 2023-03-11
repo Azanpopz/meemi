22d9325c29d67ab7c94a738e6e0fd60323426c82
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 13:19:15 +0530

    Update playstore.py

--- plugins/myplugins/playstore.py
+++ plugins/myplugins/playstore.py
@@ -1,71 +1,34 @@
-import asyncio
-from pyrogram import Client, filters
-from pyrogram.errors import QueryIdInvalid, FloodWait
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, InlineQuery, InlineQueryResultArticle, \
-    InputTextMessageContent
-
-import os
-import play_scraper
+import requests
+from dotenv import load_dotenv
+from requests.utils import requote_uri
 from pyrogram import Client, filters
 from pyrogram.types import *
 
 
+load_dotenv()
+API = "https://api.abirhasan.wtf/google?query="
+
+
 Bot = Client(
-    "Play-Store-Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    "Google-Search-Bot",
+    bot_token=os.environ.get("BOT_TOKEN"),
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH")
 )
 
 
-@Client.on_inline_query()
-async def inline_handlers(_, inline: InlineQuery):
-    results = play_scraper.search(inline.query)
-    answers = []
-    if result in results:
-        
-        answers.append(
-                InlineQueryResultArticle(
-                    title=result["title"],
-                    description=result.get("description", None),
-                    thumb_url=result.get("icon", None),
-                    input_message_content=InputTextMessageContent(
-                        message_text=details, disable_web_page_preview=True
-                    ),
-                    reply_markup=reply_markup
-                )
-            )
-
-    elif search_ts.startswith("!yts"):
-        query = search_ts.split(" ", 1)[-1]
-        if (query == "") or (query == " "):
-            answers.append(
-                InlineQueryResultArticle(
-                    title="!yts [text]",
-                    description="Search For Torrent in YTS ...",
-                    input_message_content=InputTextMessageContent(
-                        message_text="`!yts [text]`\n\nSearch YTS Torrents from Inline!",
-                        message_text=details, disable_web_page_preview=True
-                    ),
-                    reply_markup=InlineKeyboardMarkup(
-                        [[InlineKeyboardButton("Search Again", switch_inline_query_current_chat="!yts ")]])
-                )
-            )
-            try:
-                for i in range(len(results)):
-                    answers.append(
-                        InlineQueryResultArticle(
-                            details = "**Title:** `{}`".format(result["title"]) + "\n" \
-                            "**Description:** `{}`".format(result["description"]) + "\n" \
-                            "**App ID:** `{}`".format(result["app_id"]) + "\n" \
-                            "**Developer:** `{}`".format(result["developer"]) + "\n" \
-                            "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
-                            "**Score:** `{}`".format(result["score"]) + "\n" \
-                            "**Price:** `{}`".format(result["price"]) + "\n" \
-                            "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
-                            "**Free:** `{}`".format(result["free"]) + "\n" \
-                            "\n" + "Made by @FayasNoushad"
-                            
-                                
-                            
-                        
+@Client.on_message(filters.private & filters.command(["google"]))
+async def start(bot, update):    
+    r = requests.get(API + requote_uri(query))
+    informations = r.json()["results"][:50]
+    text = f"**Title:** `{info['title']}`"
+    text += f"\n**Description:** `{info['description']}`"
+    text += f"\n\nMade by @FayasNoushad"
+    buttons = [[
+        InlineKeyboardButton("JOIN MOVIES", url="https://t.me/NASRANI_UPDATE")
+    ]]                           
+    await message.reply_audio(
+    text = f"**Title:** `{info['title']}`",
+    reply_markup=InlineKeyboardMarkup(buttons)
+) 
+    return results
