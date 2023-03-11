85c4354adf3b26c6bb2358c0f31224f9249a2890
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 22:11:45 +0530

    Update caption.py

--- plugins/myplugins/caption.py
+++ plugins/myplugins/caption.py
@@ -14,12 +14,12 @@ import pyrogram, os, asyncio
 
 
 
-try: app_id = int(os.environ.get("app_id", None))
-except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
-try: api_hash = os.environ.get("api_hash", None)
-except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
-try: bot_token = os.environ.get("bot_token", None)
-except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
+try: API_ID = int(os.environ.get("API_ID", None))
+except Exception as app_id: print(f"⚠️ APP_ID Invalid {API_ID}")
+try: API_HASH = os.environ.get("api_hash", None)
+except Exception as api_id: print(f"⚠️ API_HASH Invalid {API_HASH}")
+try: BOT_TOKEN = os.environ.get("bot_token", None)
+except Exception as bot_token: print(f"⚠️ Bot Token Invalid {BOT_TOKEN}")
 try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
