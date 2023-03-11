8898c4c52f0587da92466eaf60d150caa1f88638
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 22:07:08 +0530

    Update caption.py

--- plugins/myplugins/caption.py
+++ plugins/myplugins/caption.py
@@ -37,7 +37,7 @@ def edit_caption(bot, update: pyrogram.types.Message):
           try: update.edit(custom_caption.format(file_name=motech.file_name))
           except pyrogram.errors.FloodWait as FloodWait:
               asyncio.sleep(FloodWait.value)
-              update.edit(custom_caption.format(file_name=motech.file_name, mote))
+              update.edit(custom_caption.format(file_name=motech.file_name))
       except pyrogram.errors.MessageNotModified: pass 
   else:
       return
