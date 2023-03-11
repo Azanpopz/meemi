d3bd0644a306dd60103ddd7ad1121c9dd768478a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-11 17:54:08 +0530

    Update Saavn.py

--- plugins/myplugins/Saavn.py
+++ plugins/myplugins/Saavn.py
@@ -73,7 +73,7 @@ async def song(client, message):
 
 
 @Client.on_message(filters.text & filters.chat(BATCH_GROUP))
-async def song(client, message):
+async def video(client, message):
     try:
         args = message.text.split(None)
     except:
@@ -107,5 +107,47 @@ async def song(client, message):
     os.remove(ffile)
     os.remove(thumbnail)
     await pak.delete()
+
+
+
+
+
+
+@Client.on_message(filters.text & filters.chat(BATCH_GROUP))
+async def song(client, message):
+    try:
+        args = message.text.split(None)
+    except:
+         
+        
+        return
+    pak = await message.reply('Downloading...')
+    try:
+        r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
+    except Exception as e:
+        await pak.edit(str(e))
+        return
+
+    r = requests.get(f"https://saavn.me/search/songs?query={args}&page=2&limit=2").json()
+    sname = r['data']['results'][0]['name']
+    slink = r['data']['results'][0]['downloadUrl'][4]['link']
+    ssingers = r['data']['results'][0]['primaryArtists']
+#   album_id = r.json()[0]["albumid"]
+    img = r['data']['results'][0]['image'][2]['link']
+    thumbnail = wget.download(img)
+    file = wget.download(slink)
+    ffile = file.replace("mp4", "mp3")
+    os.rename(file, ffile)
+    buttons = [[
+        InlineKeyboardButton("JOIN MOVIES", url="https://t.me/NASRANI_UPDATE")
+    ]]                           
+    await message.reply_audio(
+    audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from @nasrani_update ",thumb=thumbnail,
+    reply_markup=InlineKeyboardMarkup(buttons)
+)
+    os.remove(ffile)
+    os.remove(thumbnail)
+    await pak.delete()
+
 
 
