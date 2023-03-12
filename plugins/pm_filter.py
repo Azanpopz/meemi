3329d3240d515790c39c929782de1218d45a22e2
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-13 01:09:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2095,7 +2095,7 @@ async def auto_filter(client, msg, spoll=False):
 
 
 async def advantage_spell_chok(client, msg):
-    imdb = await get_poster(query=movie, id=True)
+    imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None)
     btn = [
             [
                 InlineKeyboardButton(
