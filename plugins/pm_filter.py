c02a189b3187a6c175716e40c7136fb52269dfe4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-13 19:15:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2107,14 +2107,17 @@ async def advantage_spell_chok(client, msg):
     settings = await get_settings(msg.chat.id)
     movies = await get_poster(mv_rqst, bulk=True)
     # plis contribute some common words
+    query = re.sub(
+        r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
+        "", msg.text, flags=re.IGNORECASE)  
     RQST = query.strip()
-    pls = query.strip() + " movie" 
+    query = query.strip() + " movie" 
     
-    imdb = await get_poster(search) if IMDB else None
+    imdb = await get_poster(query) if IMDB else None
       
     if imdb:
             caption = IMDB_TEMPLATE.format(
-                query=search,
+                query=query,
                 title=imdb['title'],
                 votes=imdb['votes'],
                 aka=imdb["aka"],
