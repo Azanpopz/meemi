d140124494bb76db29dde70937987be3f92d65b9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-13 18:49:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2106,9 +2106,7 @@ async def advantage_spell_chok(client, msg):
     reqstr = await client.get_users(reqstr1)
     settings = await get_settings(msg.chat.id)
     movies = await get_poster(mv_rqst, bulk=True)
-    query = re.sub(
-            r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
-            "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
+    # plis contribute some common words
     RQST = query.strip()
     query = query.strip() + " movie"
     imdb = await get_poster(search) if IMDB else None
