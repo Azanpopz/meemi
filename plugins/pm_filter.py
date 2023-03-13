3fe5bec7c165b49b5b15a986d95c93d04c57b772
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-13 18:59:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2146,6 +2146,18 @@ async def advantage_spell_chok(client, msg):
         
             if imdb and imdb.get('poster'):
                 try:
+                    movielist += [movie.get('title') for movie in movies]
+                    movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
+                    SPELL_CHECK[mv_id] = movielist
+                    btn = [
+                        [
+                            InlineKeyboardButton(
+                                text=movie_name.strip(),
+                                callback_data=f"spol#{reqstr1}#{k}",
+                            )
+                        ]
+                    for k, movie_name in enumerate(movielist)
+                    ]
                     await msg.reply_photo(photo=imdb['poster'], caption=caption,
                                                 reply_markup=InlineKeyboardMarkup(btn))
                 except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
