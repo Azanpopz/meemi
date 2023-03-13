efe882638039eb4a3caa79e96f179b7bf7d2e8b5
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-14 01:07:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2216,7 +2216,7 @@ async def advantage_spell_chok(client, msg):
             ]
             btn.append([InlineKeyboardButton(text="🔐𝐂𝐥𝐨𝐬𝐞🔐", callback_data=f'spol#{reqstr1}#close_spellcheck')])
             btn.insert(0, [
-                InlineKeyboardButton((f"🔰{mv_rqst}🔰", url="imdb['url']"),
+                InlineKeyboardButton(f"🔰{mv_rqst}🔰", url="imdb['url']"),
                 InlineKeyboardButton("🔍Gᴏᴏɢʟᴇ🔎", url=f"https://www.google.com/search?q={reqst_gle}")
             ])
 
