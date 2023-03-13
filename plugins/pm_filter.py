32570d02a4e0205c7b91cd681f3a03061b987e2c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-13 18:31:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2169,8 +2169,8 @@ async def advantage_spell_chok(client, msg):
 #            query = re.sub(
 #            r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
 #            "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
-#            RQST = query.strip()
-#            query = query.strip() + " movie"
+            RQST = query.strip()
+            query = query.strip() + " movie"
             try:
 #                d_msg = await msg.reply_photo(photo=imdb.get('poster'))
 #                                                  
