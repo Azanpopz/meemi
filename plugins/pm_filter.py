2a4dc43d231d459d4a81c470d163d4ddc440ce9d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-13 19:25:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2111,7 +2111,7 @@ async def advantage_spell_chok(client, msg):
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  
     RQST = see.strip()
-    see = query.strip() + " movie" 
+    see = see.strip() + " movie" 
     
     imdb = await get_poster(search) if IMDB else None
       
