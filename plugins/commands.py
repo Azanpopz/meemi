2ee65c711e3b4e30bc6ced3d0e9437cabd57d521
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-14 21:44:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -841,40 +841,40 @@ async def requests(bot, message):
                 keywords = ["#request", "/request", "#Request", "/Request"]
                 for keyword in keywords:
                     if keyword in content:
-                    content = content.replace(keyword, "")
-            try:
-                if REQST_CHANNEL is not None and len(content) >= 3:
-                    btn = [[
-                            InlineKeyboardButton('View Request', url=f"{message.link}"),
-                            InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                          ]]
-                    reported_post = await bot.send_message(chat_id=REQST_CHANNEL, text=f"😍𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})  \n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
-                    success = True
-                elif len(content) >= 3:
-                    for admin in ADMINS:
+                        content = content.replace(keyword, "")
+                try:
+                    if REQST_CHANNEL is not None and len(content) >= 3:
                         btn = [[
-                            InlineKeyboardButton('View Request', url=f"{message.link}"),
-                            InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                          ]]
-                        reported_post = await bot.send_message(chat_id=admin, text=f"🥺𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
+                                InlineKeyboardButton('View Request', url=f"{message.link}"),
+                                InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
+                              ]]
+                        reported_post = await bot.send_message(chat_id=REQST_CHANNEL, text=f"😍𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})  \n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
                         success = True
-                else:
+                    elif len(content) >= 3:
+                        for admin in ADMINS:
+                            btn = [[
+                                InlineKeyboardButton('View Request', url=f"{message.link}"),
+                                InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
+                              ]]
+                            reported_post = await bot.send_message(chat_id=admin, text=f"🥺𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
+                            success = True
+                    else:
+                        if len(content) < 3:
+                            await message.reply_text("You must type about your request [Minimum 3 Characters]. Requests can't be empty.")
                     if len(content) < 3:
-                        await message.reply_text("You must type about your request [Minimum 3 Characters]. Requests can't be empty.")
-                if len(content) < 3:
-                    success = False
-            except Exception as e:
-                await message.reply_text(f"Error: {e}")
-                pass
+                        success = False
+                except Exception as e:
+                    await message.reply_text(f"Error: {e}")
+                    pass
 
-        else:
-            success = False
+            else:
+                success = False
     
-        if success:
-            btn = [[
-                    InlineKeyboardButton('View Request', url=f"{reported_post.link}")
-                  ]]
-            await message.reply_text("<b>Your request has been added! Please wait for some time.</b>", reply_markup=InlineKeyboardMarkup(btn))
+            if success:
+                btn = [[
+                        InlineKeyboardButton('View Request', url=f"{reported_post.link}")
+                      ]]
+                await message.reply_text("<b>Your request has been added! Please wait for some time.</b>", reply_markup=InlineKeyboardMarkup(btn))
 
         
         
