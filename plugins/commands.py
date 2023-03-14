45a75e5daf777a0375f292101d35d6fa26353163
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-14 22:58:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -799,13 +799,13 @@ async def requests(bot, message):
                             InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
                           ]]
                     reported_post = await bot.send_message(chat_id=admins, text=f"🤯𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n\n 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
-                for admin in ADMINS:
-                    btn = [[
-                            InlineKeyboardButton('View Request', url=f"{message.reply_to_message.link}"),
-                            InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-                          ]]
-                   reported_post = await bot.send_message(chat_id=admin, text=f"🙂𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n \n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
-                   success = True
+                    for admin in ADMINS:
+                        btn = [[
+                                InlineKeyboardButton('View Request', url=f"{message.reply_to_message.link}"),
+                                InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
+                              ]]
+                        reported_post = await bot.send_message(chat_id=admin, text=f"🙂𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n \n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}", reply_markup=InlineKeyboardMarkup(btn))
+                        success = True
                     
                     
                                                 
