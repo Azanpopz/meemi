f14abace6a26e99ae8997cf9512f5bc7433f2e75
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-15 11:09:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2271,7 +2271,7 @@ async def advantage_spell_chok(client, msg):
         for mov in gs_parsed:
             imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
             if imdb_s:
-                movielist += [movie.get('title') [movie.get('year') for movie in imdb_s]
+                movielist += [f"🙂{movie.get('title')} {movie.get('year')}🙂"  for movie in imdb_s]
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
