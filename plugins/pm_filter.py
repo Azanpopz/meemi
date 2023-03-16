e0d51fa29a57435c480395fbb41a6c96db303d38
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-16 19:18:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1069,40 +1069,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         req = query.from_user.id
         chat_id = query.message.chat.id
         message = query.message
-        imdb = await get_poster(searchh) if IMDB else None
-
-        
-        cap = IMDB_TEMPLATE.format(
-            query=searchh,            
-            title=imdb['title'],
-            votes=imdb['votes'],
-            aka=imdb["aka"],
-            seasons=imdb["seasons"],
-            box_office=imdb['box_office'],
-            localized_title=imdb['localized_title'],
-            kind=imdb['kind'],
-            imdb_id=imdb["imdb_id"],
-            cast=imdb["cast"],
-            runtime=imdb["runtime"],
-            countries=imdb["countries"],
-            certificates=imdb["certificates"],
-            languages=imdb["languages"],
-            director=imdb["director"],
-            writer=imdb["writer"],
-            producer=imdb["producer"],
-            composer=imdb["composer"],
-            cinematographer=imdb["cinematographer"],
-            music_team=imdb["music_team"],
-            distributors=imdb["distributors"],
-            release_date=imdb['release_date'],
-            year=imdb['year'],
-            genres=imdb['genres'],
-            poster=imdb['poster'],
-            plot=imdb['plot'],
-            rating=imdb['rating'],
-            url=imdb['url'],
-            **locals()
-        )
 
         k = await query.message.edit_text(f"{query.from_user.mention}💕")
         await query.message.edit_reply_markup(reply_markup)
