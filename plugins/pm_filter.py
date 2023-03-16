d7c0917ddc998051358516f0bb6c82dadbae18b2
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-16 22:02:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -425,14 +425,15 @@ async def auto_filter(bot, query, k):
             url=imdb['url'],
            **locals()
         )
-       
-        content = query.message.reply_to_message.text
-        mention = query.message.from_user.mention
-        return await query.answer(script.OLD_ALRT_TXT.format(query.from_user.first_name), show_alert=True)
-    if int(user) != 0 and query.from_user.id != int(user):
+    if imdb and imdb.get('poster'):
+        try:      
+            content = query.message.reply_to_message.text
+            mention = query.message.from_user.mention
+            return await query.answer(script.OLD_ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+        if int(user) != 0 and query.from_user.id != int(user):
         return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
-    if movie_ == "auto":
-        return await query.message.delete()
+        if movie_ == "auto":
+            return await query.message.delete()
     
 @Client.on_callback_query(filters.regex(r"^spol"))
 async def advantage_spoll_choker(bot, query):
