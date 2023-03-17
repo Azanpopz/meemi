44f1ba3beddc7054247d4e853fd9b75208e931da
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-17 13:36:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2358,10 +2358,45 @@ async def advantage_spell_chok(client, msg):
                     )
                     await asyncio.sleep(30)
                     await k.delete()
-
+                    return
+                if AUTH_CHANNEL and not await is_subscribed(client, message):
+                    try:
+                        invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+                    except ChatAdminRequired:
+                        logger.error("Make sure Bot is admin in Forcesub channel")
+                        return
+                    btn = [
+                            [
+                               InlineKeyboardButton(
+                               "JOIN CHANNEL", url=invite_link.invite_link
+                               ),
+                               InlineKeyboardButton(
+                               text="NEW MOVIES",
+                               url="https://t.me/+cACZdXU2LH8xOGE1"
+                               ),
+                          ]
+                
+                        ]
+        
+                    if message.command[1] != "subscribe":
+                        try:
+                            kk, file_id = message.command[1].split("_", 1)
+                            pre = 'checksubp' if kk == 'filep' else 'checksub' 
+                            btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
+                        except (IndexError, ValueError):
+                            btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                    m=await message.reply_sticker("CAACAgUAAxkBAAINdmL9uWnC3ptj9YnTjFU4YGr5dtzwAAIEAAPBJDExieUdbguzyBAeBA")
+                    await asyncio.sleep(1)
+                    await m.delete()
+                    await client.send_message(
+                    chat_id=message.from_user.id,
+                    text="**PLEASE JOIN MY UPDATES CHANNEL TO USE TRY AGAIN BUTTON!**",
+                    reply_markup=InlineKeyboardMarkup(btn),
+                    parse_mode=enums.ParseMode.MARKDOWN
+                    )
 
        
-                    return
+                return
 #               movielist += [movie.get('title') for movie in movies]
                 movielist += [f"📀{movie.get('title')} {movie.get('year')}📀" for movie in movies]           
                 mv_rqst = msg.text
