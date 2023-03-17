2ec92fc4bb4561882732ca99b2387258731e9431
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-18 01:00:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -416,8 +416,8 @@ async def advantage_spoll_choker(bot, query):
     await query.answer(script.TOP_ALRT_MSG)
 #    if movie_ == "india":       
 #        await query.answer(f"{query.from_user.first_name} \n𝐋𝐨𝐚𝐝𝐢𝐧𝐠....𝐒𝐜𝐫𝐞𝐞𝐧", show_alert=True)
-         await auto_filter(bot, query, k)
-     else:
+        await auto_filter(bot, query, k)
+    else:
         _, user, movie_ = query.data.split('#')
         imdb = await get_poster(searchh) if IMDB else None
         content = query.message.reply_to_message.text
