c6b7b278bff67d5bf8ab0cec91ed826ce94e624a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-18 15:41:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2454,9 +2454,9 @@ async def advantage_spell_chok(client, msg):
                     
        
                     return
-#               movielist += [movie.get('title') for movie in movies]
+#                movielist += [movie.get('title') for movie in movies]
                 movielist += [f"📀{movie.get('title')} {movie.get('year')}📀" for movie in movies]           
-                mv_rqst = msg.text.
+                mv_rqst = msg.text
                 SPELL_CHECK[mv_id] = movielist
                 btn = [
                     [
