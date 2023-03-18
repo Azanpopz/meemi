49da66b33f6f2304f7ee093fd6c9638fd8cdf6b0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-18 17:18:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2435,11 +2435,11 @@ async def advantage_spell_chok(client, msg):
         InlineKeyboardButton(f"🔰{imdb.get('title')} - {imdb.get('year')}🔰", callback_data=f"spol#{reqstr1}#{k}")                                          
     ])
     k = await msg.reply_sticker("CAACAgUAAx0CQTCW0gABB5EUYkx6-OZS7qCQC6kNGMagdQOqozoAAgQAA8EkMTGJ5R1uC7PIECME") 
-     await asyncio.sleep(1)
-     await k.delete()           
-     await msg.reply_photo(photo=imdb['poster'], caption=caption,
-                                 reply_markup=InlineKeyboardMarkup(btn))
-     await msg.delete()
+    await asyncio.sleep(1)
+    await k.delete()           
+    await msg.reply_photo(photo=imdb['poster'], caption=caption,
+                                reply_markup=InlineKeyboardMarkup(btn))
+    await msg.delete()
  
 
 
