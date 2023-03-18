581f28e2ffd670686ae797034e8801d07d684051
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-18 17:14:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2439,18 +2439,7 @@ async def advantage_spell_chok(client, msg):
                       
 
 
-    try:
-        if settings['auto_delete']:
-            await asyncio.sleep(600)
-            await spell_check_del.delete()
-    except KeyError:
-            grpid = await active_connection(str(message.from_user.id))
-            await save_group_settings(grpid, 'auto_delete', True)
-            settings = await get_settings(message.chat.id)
-            if settings['auto_delete']:
-                await asyncio.sleep(600)
-                await spell_check_del.delete()
-
+    
 
 
 
