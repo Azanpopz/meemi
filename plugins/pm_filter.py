524d83fea50a1633eccc6e25350d9b81912e027b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-18 13:14:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2162,7 +2162,7 @@ async def auto_filter(client, msg, spoll=False):
                                          callback_data=f'{pre}#{file.file_id}')
                                         
                   ],
-                  for file in files
+                  
                 ]
 
         
