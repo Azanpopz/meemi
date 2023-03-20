52ca537479226e588a8aa46b91fd58d31c36a666
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-20 21:13:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2422,9 +2422,9 @@ async def advantage_spell_chok(client, msg):
 
         i += 1
 
-        btn.append([text, f"spol#{user}#{k}", same])
+        btn.append([text, f"spol#{reqstr1}#{k}", same])
 
-    btn.append(["❌ Close", f'spol#{user}#close_spellcheck', False])
+    btn.append(["❌ Close", f'spol#{reqstr1}#close_spellcheck', False])
     btn = build_keyboard(btn)
 
     btn.insert(0, [
