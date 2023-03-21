3c4bdc26fd5cce3c543e0922f1e6901e0a7d22fd
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 14:26:01 +0530

    Update Mdisk.py

--- plugins/modules/Mdisk.py
+++ plugins/modules/Mdisk.py
@@ -25,7 +25,7 @@ async def start(client, message):
 
 @Client.on_message(filters.command(['mdisk']))
 async def mdisk(client, message):
-    await app.send_chat_action(chat_id, enums.ChatAction.TYPING)
+    await app.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
     
     mt = message.text
     if (" " in message.text):
