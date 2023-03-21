5ca985f851eb8711fa2068ef8776de3ba738ecfc
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-22 02:38:16 +0530

    Update qutoe.py

--- plugins/myplugins/qutoe.py
+++ plugins/myplugins/qutoe.py
@@ -177,8 +177,23 @@ async def create_sticker(c: Client, m: Message):
         logging.error(e)
 
 
-@Client.on_message(filters.command(["quotes", "quote"]) & filters.reply & filters.private)
+@Client.on_message(filters.command(["quotes"]) & filters.reply & filters.private)
 async def create_sticker_private_handler(c: Client, m: Message):
     s = await m.reply_text("...", reply_to_message_id=m.message_id)
     await create_sticker(c, m.reply_to_message)
     await s.delete()
+
+@Client.on_message(filters.command(["quote"]) & filters.reply & filters.private)
+async def create_sticker_private_handler(c: Client, m: Message):
+await m.reply_sticker(
+    chat_id=m.chat.id,
+    reply_to_message_id=m.id
+)
+
+@Client.on_message(filters.command(["q"]) & filters.reply & filters.private)
+async def create_sticker_private_handler(c: Client, m: Message):
+await m.reply_sticker(
+    chat_id=m.chat.id,
+    reply_to_message_id=m.id
+)
+await create_sticker(reply_to_message_id=m.id)
