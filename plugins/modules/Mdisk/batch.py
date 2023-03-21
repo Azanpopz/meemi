61d67368c4dc823140aa2fe80d1a927c00400414
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 19:46:12 +0530

    Update batch.py

--- plugins/modules/Mdisk/batch.py
+++ plugins/modules/Mdisk/batch.py
@@ -1,138 +1,153 @@
 import asyncio
-from translation import BATCH
-from plugins.modules.Mdisk.helpers import AsyncIter, temp
-from pyrogram import Client, filters
-from util import main_convertor_handler
-from config import ADMINS, SOURCE_CODE
-from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+import json
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
-import datetime
+from config import CHANNELS, ADMINS, SOURCE_CODE
+from util import replace_mdisk_link, caption
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+import os
+import sys
+from pyrogram import Client, filters
+from translation import BATCH
 
-# Logger
-import logging
-logger = logging.getLogger(__name__)
-
-
-lock = asyncio.Lock()
+buttons = [
+    [
+        InlineKeyboardButton('Batch Short 🏕', callback_data='batch')
+    ],
+    [
+        InlineKeyboardButton('Cancel 🔐', callback_data='cancel')
+    ]
+]
 
 cancel_button = [[
     InlineKeyboardButton('Cancel 🔐', callback_data='cancel_process')
 ]
 ]
 
+channel_id = ""
 
-@Client.on_message(filters.command('mbatch'))
+
+@Client.on_message(filters.private & filters.command('mdisk'))
 async def batch(c, m):
-
     if m.from_user.id in ADMINS:
-        if len(m.command) < 2:
-            await m.reply_text(BATCH)
-        else:
-            channel_id = m.command[1]
-            if channel_id.startswith("@"):
-                channel_id = channel_id.split("@")[1]
-            elif channel_id.startswith("-100"):
-                channel_id = int(channel_id)
 
-            buttons = [
-[
-InlineKeyboardButton('Batch Short 🏕', callback_data=f'batch#{str(channel_id)}')
-],
-[
-InlineKeyboardButton('Cancel 🔐', callback_data='cancel')
-]
-]
+        global channel_id
+        print("Started")
+        if CHANNELS is True:
+            if len(m.command) < 2:
+                await m.reply_text(BATCH)
+            else:
+                channel_id = m.command[1]
+                if channel_id.startswith("@"):
+                    channel_id = channel_id.split("@")[1]
+                elif channel_id.startswith("-100"):
+                    channel_id = int(channel_id)
+                elif channel_id.startswith("t.me"):
+                    channel_id = channel_id.split("/")[-1]
+                    if channel_id.startswith(("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")):
+                        channel_id = int(channel_id)
+                    else:
+                        channel_id = str(channel_id)
+                elif channel_id.startswith("https"):
+                    channel_id = channel_id.split("/")[-1]
 
-            return await m.reply(text=f"Are you sure you want to batch short?\n\nChannel: {channel_id}", reply_markup=InlineKeyboardMarkup(buttons))
+                await m.reply(text=f"Are you sure you want to batch short?\n\nChannel: {channel_id}",
+                              reply_markup=InlineKeyboardMarkup(buttons))
+                print(channel_id)
 
+        elif CHANNELS is False:
+            await m.reply(text="Set your CHANNELS var to True in HEROKU to use this command")
     elif m.from_user.id not in ADMINS:
-        await m.reply_text(f"""This bot works only for ADMINS of this bot. Make your own Bot.\n\n[Source Code]({SOURCE_CODE})""")
+        await m.reply_text(
+                f"""This bot works only for ADMINS of this bot. Make your own Bot.\n\n[Source Code]({SOURCE_CODE})""")
 
 
 @Client.on_callback_query(filters.regex(r'^cancel') | filters.regex(r'^batch'))
-async def batch_handler(c:Client, m:CallbackQuery):
+async def cancel(c, m):
 
-    user_method = 'mdisk'
-
+    global channel_id
     if m.data == "cancel":
         await m.message.delete()
-        return
-    elif m.data.startswith('batch'): 
-        if lock.locked():
-            return await m.answer('Wait until previous process complete.', show_alert=True)
-            
-        channel_id = int(m.data.split('#')[1])
-        try:
-            txt = await c.send_message(channel_id, ".")
-            id = txt.id
-            await txt.delete()
-        except ChatWriteForbidden:
-            return await m.message.edit("Bot is not an admin in the given channel")
-        except PeerIdInvalid:
-            return await m.message.edit("Given channel ID is invalid")
+    elif m.data == "batch":
+        if CHANNELS is True:
+            try:
+                txt = await c.send_message(channel_id, ".")
+                await txt.delete()
+                print(txt.id)
+            except ChatWriteForbidden:
+                await m.message.edit("Bot is not an admin in the given channel")
+            start_msg = await m.message.edit(text=f"Batch Shortening Started!\n\n Channel: {channel_id}\n\nTo Cancel /cancel",
+                                 )
 
-        start_time = datetime.datetime.now()
+            for i in range(txt.id, 1, -1):
+                try:
+                    message = await c.get_messages(channel_id, i)
 
-        txt = await m.message.edit(text=f"Batch Shortening Started!\n\n Channel: {channel_id}\n\nTo Cancel /cancel",)
+                    if message.forward_from:
+                        return
 
-        logger.info(f"Batch Shortening Started for {channel_id}")
+                    if message.reply_markup:
+                        reply_markup = json.loads(str(message.reply_markup))
+                        buttsons = []
+                        for i, markup in enumerate(reply_markup["inline_keyboard"]):
+                            buttons = []
+                            for j in markup:
+                                text = j["text"]
+                                url = j["url"]
+                                url = await replace_mdisk_link(url)
+                                button = InlineKeyboardButton(text, url=url)
+                                buttons.append(button)
+                            buttsons.append(buttons)
 
-        success = 0
-        fail = 0
-        total = 0
-        empty=0
+                        try:
+                            if message.text:
+                                txt = await replace_mdisk_link(txt)
+                                await message.edit_text(text=txt, reply_markup=InlineKeyboardMarkup(buttsons))
+                            elif message.caption:
+                                txt = await replace_mdisk_link(message.caption)
+                                if message.photo:
+                                    await message.edit_caption(photo=message.photo.file_id, caption=txt,
+                                                               reply_markup=InlineKeyboardMarkup(buttsons),
 
-        total_messages = (range(1,id))
+                                                               )
+                                elif message.document:
+                                    await message.edit_caption(photo=message.document.file_id, caption=txt,
+                                                               reply_markup=InlineKeyboardMarkup(buttsons),
+                                                               )
+                        except Exception as e:
+                            print(e)
 
-        try:
-            for i in range(0, len(total_messages), 200):
-                channel_posts = AsyncIter(await c.get_messages(channel_id, total_messages[i:i+200]))
+                    # For text messages
 
-                temp.CANCEL = False
+                    elif message.text:
+                        text = message.text
+                        text = await replace_mdisk_link(text)
+                        await message.edit_text(text)
 
-                async with lock:
-                        async for message in channel_posts:
-                            if temp.CANCEL == True:
-                                break
+                    # For media or document messages
 
-                            if message.media or message.text:
-                                try:
-                                    await main_convertor_handler(message=message, type=user_method, edit_caption=True)
-                                    success += 1
-                                except Exception as e:
-                                    logger.error(e)
-                                    fail+=1
-                                await asyncio.sleep(1)
-                            else:
-                                empty += 1
-                            total+=1
+                    elif message.media or message.document:
+                        text = message.caption
+                        link = await replace_mdisk_link(text)
+                        if link == text:
+                            print("The given link is either excluded domain link or a droplink link")
+                        else:
+                            await message.edit_caption(link)
+                    await asyncio.sleep(1)
 
-                            if total % 10 == 0:
-                                msg = f"Batch Shortening in Process !\n\nTotal: {total}\nSuccess: {success}\nFailed: {fail}\nEmpty: {empty}\n\nTo cancel the batch: /cancel"
-                                await txt.edit((msg))
-        except Exception as e:
-            logger.error(e)
-            await m.message.reply("Error Occured while processing batch: `%s`" % e.message)
-        finally:
-            end_time = datetime.datetime.now()
-            await asyncio.sleep(10)
-            t = end_time - start_time
-            time_taken = str(datetime.timedelta(seconds=t.seconds))
-            msg = f"Batch Shortening Completed!\n\nTime Taken - `{time_taken}`\n\nTotal: `{total}`\nSuccess: `{success}`\nFailed: `{fail}`\nEmpty: `{empty}`"
-            await txt.edit(msg)
-            logger.info(f"Batch Shortening Completed for {channel_id}")
+                except Exception as e:
+                    print(e)
 
+            await start_msg.edit("BATCH Shortening Completed")
 
 
 @Client.on_message(filters.command('cancel'))
 async def stop_button(c, m):
     if m.from_user.id in ADMINS:
-        temp.CANCEL = True
+        print("Cancelled")
         msg = await c.send_message(
-            text="<i>Trying To Stoping.....</i>",
-            chat_id=m.chat.id
+                text="<i>Trying To Stoping.....</i>",
+                chat_id=m.chat.id
         )
         await asyncio.sleep(5)
         await msg.edit("Batch Shortening Stopped Successfully 👍")
-        logger.info(f"Batch Shortening Stopped Successfully 👍")
+        os.execl(sys.executable, sys.executable, *sys.argv)
