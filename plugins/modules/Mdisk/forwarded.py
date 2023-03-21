19f3f51dceb69e2a5781956e0a45eab95a72628a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 19:31:36 +0530

    Update forwarded.py

--- plugins/modules/Mdisk/forwarded.py
+++ plugins/modules/Mdisk/forwarded.py
@@ -1,11 +1,11 @@
 # short and repost forwarded message
 
 from pyrogram import Client, filters
-from config import CHANNEL_ID, FORWARD_MESSAGE, CHANNELS
+from config import MDISK_CHANNEL, FORWARD_MESSAGE, CHANNELS
 from util import main_convertor_handler
 
 
-@Client.on_message(filters.chat(CHANNEL_ID) & (
+@Client.on_message(filters.chat(MDISK_CHANNEL) & (
         filters.channel | filters.group) & filters.incoming & ~filters.private & filters.forwarded)
 async def channel_forward_link_handler(c:Client, message):
     if FORWARD_MESSAGE == "True" or FORWARD_MESSAGE is True and CHANNELS is True or CHANNELS == 'True':
