b9f094d77f4c451e32ad435d27bdf0adf35ec14b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 19:30:45 +0530

    Update channels.py

--- plugins/modules/Mdisk/channels.py
+++ plugins/modules/Mdisk/channels.py
@@ -1,10 +1,10 @@
-from config import CHANNEL_ID, CHANNELS
+from config import MDISK_CHANNEL, CHANNELS
 from pyrogram import Client, filters
 from util import main_convertor_handler
 
 
 # Channel
-@Client.on_message(~filters.forwarded & filters.chat(CHANNEL_ID) & (
+@Client.on_message(~filters.forwarded & filters.chat(MDISK_CHANNEL) & (
                 filters.channel | filters.group) & filters.incoming & ~filters.private &
         ~filters.forwarded)
 async def channel_link_handler(c:Client, message):
