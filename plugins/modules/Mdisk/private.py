786e1550c7f049d2709f2a0f9f39596cf5c8712b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 21:30:45 +0530

    Update private.py

--- plugins/modules/Mdisk/private.py
+++ plugins/modules/Mdisk/private.py
@@ -16,8 +16,8 @@ from info import BATCH_GROUP
 
 # Private Chat
 
-@Client.on_message(filters.command("m") & filters.chat(BATCH_GROUP) & (
-        filters.channel | filters.group) & filters.incoming & filters.forwarded)
+@Client.on_message(filters.command("m") & filters.chat(BATCH_GROUP) & (filters.channel | filters.group) & filters.incoming & filters.forwarded)
+        
 
 
 #@Client.on_message(filters.command("m") & filters.text & filters.photo & filters.chat(BATCH_GROUP))
