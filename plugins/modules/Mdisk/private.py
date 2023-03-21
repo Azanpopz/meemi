3f7b8afc224c204b744a87c9fae0cbad457fbdd5
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 19:15:17 +0530

    Update and rename plugins/modules/Mdisk.py to plugins/modules/Mdisk/private.py

---
+++ plugins/modules/Mdisk/private.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+from util import main_convertor_handler
+from config import ADMINS, SOURCE_CODE
+from pyrogram.types import Message
+
+
+
+# Private Chat
+@Client.on_message(filters.private & filters.incoming)
+async def private_link_handler(c, message):
+    if message.from_user.id not in ADMINS:
+        return await message.reply_text(f"This bot works only for ADMINS of this bot. Make your own Bot.\n\n[Source Code]({SOURCE_CODE})")
+        
+    try:
+        txt = await message.reply('`Cooking... It will take some time if you have enabled Link Bypass`', quote=True)
+        await main_convertor_handler(message, 'mdisk')
+
+        # Updating DB stats
+    except Exception as e:
+        await message.reply("Error while trying to convert links %s:" % e, quote=True)
+    finally:
+        await txt.delete()
+
