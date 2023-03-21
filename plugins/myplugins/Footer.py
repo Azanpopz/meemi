d1bb63ea27a72c2db186fa3e68725ad8150f8488
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 23:12:50 +0530

    Update Footer.py

--- plugins/myplugins/Footer.py
+++ plugins/myplugins/Footer.py
@@ -7,20 +7,22 @@ import os
 import time
 from Script import script
 from pyrogram.types import Message
-from database.Db import get_user
+from database.users_chats_db import add_user
+
+
 
 @Client.on_message(filters.command('footer') & filters.private)
 async def footer_handler(client, message):
     user_id = message.from_user.id
     cmd = message.command
-    user = await get_user(user_id)
+    user = add_user(user_id)
     if not message.reply_to_message:
         if "remove" not in cmd:
             return await message.reply(PICS + "\n\nCurrent Footer Text: " + user["footer_text"].replace("\n", "\n"))
 
-        await update_user_info(user_id, {"footer_text": ""})
+        await add_user(user_id, {"footer_text": ""})
         return await message.reply("Footer Text Successfully Removed")
-    elif m.reply_to_message.text:
+    elif message.reply_to_message.text:
         footer_text = message.reply_to_message.text.html
-        await update_user_info(user_id, {"footer_text": footer_text})
+        await add_user(user_id, {"footer_text": footer_text})
         await message.reply("Footer Text Updated Successfully")
