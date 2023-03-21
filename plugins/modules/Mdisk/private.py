217dc479438bcec8e4d39f6f9f78f68e676570f8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 20:31:33 +0530

    Update private.py

--- plugins/modules/Mdisk/private.py
+++ plugins/modules/Mdisk/private.py
@@ -16,7 +16,7 @@ from info import BATCH_GROUP
 
 # Private Chat
 
-@Client.on_message(filters.text & filters.chat(BATCH_GROUP))
+@Client.on_message(filters.command("m") & filters.text & filters.chat(BATCH_GROUP))
 async def private_link_handler(c, message):
     if message.from_user.id not in ADMINS:
         return await message.reply_text(f"This bot works only for ADMINS of this bot. Make your own Bot.\n\n[Source Code]({SOURCE_CODE})")
