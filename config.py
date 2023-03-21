809fc2a627fc2bb12450ca397517a5fd90faf3b2
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 22:27:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+import os
 
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
@@ -22,5 +25,25 @@ CHANNELS = bool(os.environ.get("CHANNELS"))
 
 
 
+
+
+
+# Mandatory variables for the bot to start
+API_ID = int(os.getenv("API_ID", "Your Api Id"))
+API_HASH = os.environ.get("API_HASH", "Your Api Hash")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+ADMIN = ADMINS
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
+DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
+ADMINS.append(Id Owned Id)
+#  Optionnal variables
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
+BATCH_GROUP = os.environ.get("BATCH_GROUP", "Batch Group User name Without @") # For Force Subscription
+BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
+LINK_BYPASS = "False"
 
 
