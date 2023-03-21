6ea6711be6732cd2534c13d3550c6acacb7ebf35
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-22 04:06:25 +0530

    Update sticker.py

--- plugins/myplugins/sticker.py
+++ plugins/myplugins/sticker.py
@@ -105,9 +105,7 @@ async def findsticker(bot, message):
         chat_id = str(message.chat.id)
         await txt.delete()
         await bot.send_sticker(chat_id,f"{stickerid}")
-    else:
-        await message.reply_text("Not a Valid File ID")
-      
+     
     except Exception as error:
         txt = await message.reply_text("Not a Valid File ID")
 
