73788c9523445765e1e72189cbec167c291abea8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 22:42:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,19 +28,4 @@ CHANNELS = bool(os.environ.get("CHANNELS"))
 
 
 
-# Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
-ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-#  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-BATCH_GROUP = os.environ.get("BATCH_GROUP", "Batch Group User name Without @") # For Force Subscription
-BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
-LINK_BYPASS = "False"
-
 
