b2302589931e39ed01c646144c389da7a30a96f1
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-22 02:55:26 +0530

    Update qutoe.py

--- plugins/myplugins/qutoe.py
+++ plugins/myplugins/qutoe.py
@@ -184,9 +184,8 @@ async def create_sticker_private_handler(c: Client, m: Message):
     await s.delete()
 
 
-@Client.on_message(filters.text & filters.private & (~filters.command("start") | ~filters.command("help")))
-async def create_sticker_private_handler(c: Client, m: Message):
-    s = await m.reply_text("...")
-    await create_sticker(c, m)
+@Client.on_message(filters.command(["sticker", "s"]) & filters.reply & filters.group)
+async def create_sticker_group_handler(c: Client, m: Message):
+    s = await m.reply_text("...", reply_to_message_id=m.message_id)
+    await create_sticker(c, m.reply_to_message)
     await s.delete()
-
