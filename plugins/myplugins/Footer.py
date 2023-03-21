577f90a429e80fe068581de9c1a8ace40ab75599
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 23:00:57 +0530

    Update Footer.py

--- plugins/myplugins/Footer.py
+++ plugins/myplugins/Footer.py
@@ -7,7 +7,7 @@ import os
 import time
 from Script import script
 from pyrogram.types import Message
-
+from database.Db import get_user
 
 @Client.on_message(filters.command('footer') & filters.private)
 async def footer_handler(client, message):
