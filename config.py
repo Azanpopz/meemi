6073c4fcc88b1db9d2bde4309d6ae0870f626410
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-21 16:50:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,20 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+
+# mdisk
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+MDISK_API = os.environ.get("MDISK_API")
+MDISK_CHANNEL = list(int(i.strip()) for i in os.environ.get("MDISK_CHANNEL").split(" ")) if os.environ.get("CHANNEL_ID") else []
+FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
+ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
+SOURCE_CODE = "💕SHARE AND SUPPORT💕"
+CHANNELS = bool(os.environ.get("CHANNELS"))
+
+
+
+
+
