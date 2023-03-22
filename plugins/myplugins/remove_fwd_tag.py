ea6bf6d5c73bc3dd1969488c4e64792b893d0dfa
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-22 21:19:21 +0530

    Update remove_fwd_tag.py

--- plugins/myplugins/remove_fwd_tag.py
+++ plugins/myplugins/remove_fwd_tag.py
@@ -4,6 +4,13 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User
 
 
+bot = Client(
+    "Remove FwdTag",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
 @Client.on_message(filters.forwarded & filters.channel & filters.group & filters.incoming)
 async def channel_tag(bot, message):
     try:
@@ -12,3 +19,36 @@ async def channel_tag(bot, message):
         await message.delete()
     except:
         await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
+
+
+
+
+@Client.on_message(filters.group & filters.text | filters.media )
+async def tag(client, message):
+ await message.copy(message.chat.id)
+
+
+
+@Client.on_message(filters.channel & filters.forwarded)
+async def fwdrmv(c, m):
+    try:
+        if m.media and not (m.video_note or m.sticker):
+            await m.copy(m.chat.id, caption = m.caption if m.caption else None)
+            await m.delete()
+        else:
+            await m.copy(m.chat.id)
+            await m.delete()
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+
+
+@Client.on_message(filters.private | filters.group)
+async def fwdrm(c, m):
+    try:
+        if m.media and not (m.video_note or m.sticker):
+            await m.copy(m.chat.id, caption = m.caption if m.caption else None)
+        else:
+            await m.copy(m.chat.id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+
