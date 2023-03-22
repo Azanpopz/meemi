26ee6d253faf2ed6fca40f5900d4f16be507ff6b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-22 12:53:00 +0530

    Update q.py

--- plugins/myplugins/q.py
+++ plugins/myplugins/q.py
@@ -17,6 +17,23 @@ from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 
+def split_limits(text):
+    if len(text) < 2048:
+        return [text]
+
+    lines = text.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+    else:
+        result.append(small_msg)
+
+    return result
 
 
 
