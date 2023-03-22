d4ec07a4c9dd589074cc8b4be7bdeb089c8f987d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-22 21:03:29 +0530

    Update remove_fwd_tag.py

--- plugins/myplugins/remove_fwd_tag.py
+++ plugins/myplugins/remove_fwd_tag.py
@@ -8,12 +8,12 @@ from pyrogram import Client, filters
 from info import BOT_TOKEN, API_ID, API_HASH, CHANNELS
 
 
-Bot = Client(
-     os.environ.get("SESSION_NAME", "No-Forward-Messages"),
-     bot_token = os.environ["BOT_TOKEN"],
-     api_id = int(os.environ["API_ID"]),
-     api_hash = os.environ["API_HASH"]
-)
+# Bot = Client(
+#     os.environ.get("SESSION_NAME", "No-Forward-Messages"),
+#     bot_token = os.environ["BOT_TOKEN"],
+#     api_id = int(os.environ["API_ID"]),
+#     api_hash = os.environ["API_HASH"]
+# )
 
 @Client.on_message(filters.forwarded & filters.group & filters.channel & filters.incoming)
 async def channel_tag(bot, message):
