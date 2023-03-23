478fa7f17f062cc0c5681950efee79713b2da940
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-23 20:45:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2272,7 +2272,7 @@ async def advantage_spell_chok(client, msg):
     ])
 
     
-    await k.delete()
+    
     spell_check_del = await msg.reply_photo(
         photo=(SPELL_IMG),
         caption=(script.CUDNT_FND.format(mv_rqst)),
