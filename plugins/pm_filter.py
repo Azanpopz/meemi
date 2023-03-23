96eb8d7ce7760bdc905f5214b7d5c8a7a2a1d763
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-24 00:17:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2284,18 +2284,10 @@ async def advantage_spell_chok(client, msg):
     ])
     btn.insert(3, [
         InlineKeyboardButton(f"{imdb.get('title')} 𝐑𝐞𝐥𝐞𝐚𝐬𝐞 {imdb.get('release_date')}🌲", callback_data=f"spol#{reqstr1}#{k}")
+    
     btn.insert(4, [
-        InlineKeyboardButton(
-            text=f"ok",
-            callback_data=f"spol#{user}#{k}",
-        )
-    ] for k, movie in enumerate(movielist))
-    btn.insert(4, [
-        InlineKeyboardButton(
-            text=f"ok🤯",
-            callback_data=f"spol#{reqstr1}#{k}",
-        )
-    ] for k, movie in enumerate(movielist))
+        InlineKeyboardButton(f"📤{imdb.get('title')} 𝐘𝐞𝐚𝐫 {imdb.get('year')}📤", callback_data=f"spol#{user}#{k}")
+    ]for k, movie in enumerate(movielist))
 
     k = await msg.reply_sticker("CAACAgUAAx0CQTCW0gABB5EUYkx6-OZS7qCQC6kNGMagdQOqozoAAgQAA8EkMTGJ5R1uC7PIECME") 
     
