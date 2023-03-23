c583cafceacf04d1dca642f5dc02a2f0ca67a4e9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-24 03:41:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1915,7 +1915,7 @@ async def auto_filter(client, msg, spoll=False):
 #        await asyncio.sleep(1)
 #        await m.delete()
         await message.reply_text(
-            chat_id=message.chat.id,
+            chat_id,
             text=f"𝐇𝐞𝐲 **𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐚𝐧𝐝 𝐑𝐞𝐪𝐮𝐬𝐭 𝐓𝐡𝐢𝐬 𝐆𝐫𝐨𝐮𝐩\n\nജോയിൻ ചെയ്തതിനു ശേഷം റിക്വസ്റ്റ് അയക്കുക**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
