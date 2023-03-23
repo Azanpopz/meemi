920638252e8d01a1c3088d39e606f9bd7a5b7f76
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-23 22:49:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2205,7 +2205,6 @@ async def advantage_spell_chok(client, msg):
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
     RQST = query.strip()
     query = query.strip() + " movie"
-    query = query.strip() + " year"
     try:
         movies = await get_poster(mv_rqst, bulk=True)
     except Exception as e:
@@ -2232,8 +2231,8 @@ async def advantage_spell_chok(client, msg):
         await k.delete()
                
         return
-#    movielist += [movie.get('title') [movie.get('year') for movie in movies]
-    movielist += [f"🔹{movie.get('title')} {movie.get('year')}🔹" for movie in movies]
+#    movielist += [movie.get('title') for movie in movies]
+    movielist += [f"🔹{movie.get('title')}🔹" for movie in movies]
     SPELL_CHECK[mv_id] = movielist
     chat_id = msg.chat.id
     mv_rqst = msg.text
