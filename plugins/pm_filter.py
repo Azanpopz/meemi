eed5e80a92c534bca1f72b85ea6cfeed98075342
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-25 00:01:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2655,7 +2655,7 @@ async def advantage_spell_chok(client, msg):
         for k, movie_name in enumerate(movielist)
     ]
     btn.append([InlineKeyboardButton(text="✘ ᴄʟᴏsᴇ ✘", callback_data=f'spol#{reqstr1}#close_spellcheck')])
-    await msg.reply_photo(photo=imdb['poster'], caption=caption,
+    await msg.reply_photo(photo=imdb['poster'], caption=(script.CUDNT_FND.format(mv_rqst)),
                                 reply_markup=InlineKeyboardMarkup(btn))
     await msg.delete()
 
