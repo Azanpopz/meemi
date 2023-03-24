1c59df8fb32f6f105076ffb33ea8ab4251c1234f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-25 00:44:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -368,7 +368,7 @@ async def advantage_spoll_choker(bot, query):
             reqstr1 = query.from_user.id if query.from_user else 0
             reqstr = await bot.get_users(reqstr1)
             if NO_RESULTS_MSG:
-                await query.answer(searching...., show_alert=True)
+                await query.answer("f"searching....", show_alert=True)
             return
                 await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
             k = await query.message.edit(script.MVE_NT_FND)
