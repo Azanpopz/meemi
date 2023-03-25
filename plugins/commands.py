d90bf1bba0d9199f700f03b349ae8a79f713e10c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-25 19:34:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -34,7 +34,7 @@ force_channel = "nasrani_batch_store"
 async def start(client, message):
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+            invite_link = await client.create_chat_invite_link(int(LOGIN_CHANNEL))
         except ChatAdminRequired:
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
