2ab1afe6793fdc3c37b5fc7f824e55f73f106f70
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-25 16:34:16 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -78,6 +78,56 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
         except KeyError:
             await save_group_settings(int(chat_id), 'max_btn', False)
             settings = await get_settings(int(chat_id))
+            max_results = 10 if settings['max_btn'] else int(MAX_B_TN)        
+    query = query.strip()
+    if not query:
+        raw_pattern = '.'
+    elif ' ' not in query:
+        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+    else:
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+
+    try:
+        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
+    except:
+        return []
+
+    if USE_CAPTION_FILTER:
+        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+    else:
+        filter = {'file_name': regex}
+
+    if file_type:
+        filter['file_type'] = file_type
+
+    total_results = await Media.count_documents(filter)
+    next_offset = offset + max_results
+
+    if next_offset > total_results:
+        next_offset = ''
+
+    cursor = Media.find(filter)
+    # Sort by recent
+    cursor.sort('$natural', -1)
+    # Slice files according to offset and max results
+    cursor.skip(offset).limit(max_results)
+    # Get list of files
+    files = await cursor.to_list(length=max_results)
+
+    return files, next_offset, total_results
+
+
+# MAX_B
+
+async def max_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
+    """For given query return (results, next_offset)"""
+    if chat_id is not None:
+        settings = await get_settings(int(chat_id))
+        try:
+            max_results = 20 if settings['max_btn'] else int(MAX_B)
+        except KeyError:
+            await save_group_settings(int(chat_id), 'max_btn', False)
+            settings = await get_settings(int(chat_id))
             max_results = 20 if settings['max_btn'] else int(MAX_B)        
     query = query.strip()
     if not query:
@@ -115,6 +165,10 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     files = await cursor.to_list(length=max_results)
 
     return files, next_offset, total_results
+
+
+
+
 
 async def get_bad_files(query, file_type=None, max_results=1000, offset=0, filter=False):
     """For given query return (results, next_offset)"""
