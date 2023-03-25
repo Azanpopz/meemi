eba37f8c07d61e16a598584ba9971f8b5738641e
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-25 14:38:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2478,7 +2478,7 @@ async def auto_filter(client, msg, spoll=False):
                             caption=f"⚙️ {message.from_user.mention} Fɪʟᴛᴇʀ Fᴏʀ {search} Cʟᴏꜱᴇᴅ 🗑️")
                             await asyncio.sleep(37)                   
                             await thega.delete()
-                except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
                     pic = imdb.get('poster')
                     poster = pic.replace('.jpg', "._V1_UX360.jpg")
                     fmsg = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
