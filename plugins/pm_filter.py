95814086f3271ad2c77e3065422cad05c45de168
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-26 15:50:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -87,14 +87,13 @@ async def fil_mod(client, message):
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     settings = await get_settings(message.chat.id)
-    if settings['auto_ffilter']:
-        await auto_filter(client, message)
-    except KeyError:
-        grpid = await active_connection(str(message.from_user.id))
-        await save_group_settings(grpid, 'auto_ffilter', True)
-        settings = await get_settings(message.chat.id)
-        if settings['auto_ffilter']:
-            await auto_filter(client, message)
+    if settings["auto_ffilter"]:
+        userid = message.from_user.id if message.from_user else None
+        if not userid:
+            search = message.text
+            k = await message.reply(f"You'r anonymous admin! Sorry you can't get '{search}' from here.\nYou can get '{search}' from bot inline search.")
+            await asyncio.sleep(30)
+            await k.delete()
             try:
                 await message.delete()
             except:
