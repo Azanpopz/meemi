5f5788db10f6f8c1ce0491938735b87a1f72956b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-26 21:51:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -109,9 +109,9 @@ async def give_filter(client, message):
                 pass
             return
 
-        if AUTH_CHANNEL and not await mute_login(client, message):
+        if LOGIN_CHANNEL and not await mute_login(client, message):
             try:
-                invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+                invite_link = await client.create_chat_invite_link(int(LOGIN_CHANNEL))
             except ChatAdminRequired:
                 logger.error("Make sure Bot is admin in Forcesub channel")
                 return
