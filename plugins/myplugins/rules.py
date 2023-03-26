30aab27ced23b18c0a322818a2d64274e65b3f89
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-27 02:38:57 +0530

    Update rules.py

--- plugins/myplugins/rules.py
+++ plugins/myplugins/rules.py
@@ -18,7 +18,12 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 
-
+Muhammed = Client(
+    "Pyrogram Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
 
 ALL_PIC = [
  "https://telegra.ph/file/d6693066f82ed4079c528.jpg",
@@ -32,12 +37,7 @@ H𝙻𝙾 {} 𝙱𝚁𝙾𝙷
 ᗰ𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/pyogram_bot'>ᴅᴀᴠᴏᴏᴅ ɪʙʀᴀʜɪᴍ⚡️</a>
 𝚃𝙷𝙸𝚂 𝙱𝙾𝚃 𝙸𝚂 𝙵𝙸𝚁𝚂𝚃 𝙾𝚆𝙽 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 𝙱𝙾𝚃 𝙾𝙵 𝙼𝚈 𝙾𝚆𝙽𝙴𝚁 𝚂𝙾 𝚃𝙷𝙴 𝙱𝙾𝚃 𝙸𝚂 𝙾𝙽 𝚃𝙷𝙴 𝚆𝙾𝚁𝙺𝚂𝙷𝙾𝙿 𝙾𝙽 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 𝙵𝙾𝚁 𝚄𝙿𝙳𝙰𝚃𝙸𝙽𝙶 𝙵𝙴𝙰𝚃𝚄𝚁𝙴𝚂 𝚂𝙾 𝙿𝙻𝙴𝙰𝚉𝙴 𝚆𝙰𝙸𝚃 𝙺𝙸𝙽𝙳𝙵𝚄𝙻𝙻𝚈...
 """
-Muhammed = Client(
-    "Pyrogram Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
-)
+
 
 
 @Client.on_message(filters.command("rule")) 
@@ -56,7 +56,7 @@ async def start_message(bot, message):
 
 @Client.on_callback_query()
 async def callback(bot: Doctor, query: CallbackQuery):
-    if query.data== "start":
+    if query.data== "rule":
         await query.message.edit(
             text=START_MESSAGE.format(query.from_user.mention)
         )
