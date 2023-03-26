0076dd9492a7f75b5402e548c942e78b22effb0e
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-27 01:22:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1888,6 +1888,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 
 async def auto_filter(client, msg, spoll=False):
+    reqstr1 = msg.from_user.id if msg.from_user else 0
+    reqstr = await client.get_users(reqstr1)
 
 #    chat_id = msg.chat.id
 #    mv_rqst = msg.text
