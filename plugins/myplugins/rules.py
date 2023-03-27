4860a19541e8b981c9ca66e7f45ba9b81f3a916b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-27 12:18:55 +0530

    Update rules.py

--- plugins/myplugins/rules.py
+++ plugins/myplugins/rules.py
@@ -106,6 +106,25 @@ async def start_message(client, message):
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await msg.reply_text(text=f"LOADING....")
+            buttons = [[
+                InlineKeyboardButton('𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ], [
+                    
+                InlineKeyboardButton('𝐔𝐩𝐝𝐚𝐭𝐞', url='https://t.me/bigmoviesworld'),
+                InlineKeyboardButton('𝐂𝐡𝐚𝐧𝐧𝐞𝐥', url='https://t.me/NasraniChatGroup')
+            ], [
+                InlineKeyboardButton('𝐁𝐨𝐭 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬', url='http://telegra.ph/Minnal-murali-03-06-12')
+            ], [
+                InlineKeyboardButton('𝐒𝐨𝐧𝐠 𝐆𝐫𝐨𝐮𝐩', url='https://t.me/nasrani_batch_store'),
+                InlineKeyboardButton('𝐌𝐨𝐯𝐢𝐞𝐬 𝐆𝐫𝐨𝐮𝐩', url='https://t.me/nasrani_update')
+            ], [
+                InlineKeyboardButton('𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩', url='https://t.me/nasrani_update')
+            ], [
+                InlineKeyboardButton('🔹🔸𝐂𝐋𝐎𝐒𝐄🔸🔹', callback_data='close_data')
+            ]]
+            hmm = await message.reply_photo(photo=poster,  caption=START_MESSAGE.format(message.from_user.mention, message.chat.title),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+            )
         except Exception as e:
             logger.exception(e)
