3613e907c8b7f8bbc1e87a2911c3bd12a01e9482
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-27 16:07:02 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -86,22 +86,4 @@ async def broadcast_messages(user_id, message):
         usernamex = usern.username
         print(usernamex)
         await bot.send_message(chat_id=int(user['id']), text=b_msg.text+"\n" "@"+usernamex)
-    try:
-        await message.copy(chat_id=user_id)
-        return True, "Success"
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return await broadcast_messages(user_id, message)
-    except InputUserDeactivated:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id}-Removed from Database, since deleted account.")
-        return False, "Deleted"
-    except UserIsBlocked:
-        logging.info(f"{user_id} -Blocked the bot.")
-        return False, "Blocked"
-    except PeerIdInvalid:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id} - PeerIdInvalid")
-        return False, "Error"
-    except Exception as e:
-        return False, "Error"
+    
