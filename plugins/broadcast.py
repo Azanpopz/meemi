90fc65e1ab36dcce8c178b657b0c76c30d1fe46c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-27 15:21:24 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -10,6 +10,7 @@ import asyncio
 # https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
     users = await db.get_all_users()
+    name =  await bot.get_users(id).first_name
     b_msg = message.reply_to_message
     sts = await message.reply_text(
         text='Broadcasting your messages...'
@@ -23,7 +24,7 @@ async def verupikkals(bot, message):
 
     success = 0
     async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        pti, sh = await broadcast_messages(int(user['id']), b_msg, <a href=tg://user?id={id}>{name}</a>)
         if pti:
             success += 1
         elif pti == False:
