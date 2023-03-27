947f60b791539cdbb3a2ed08157959500bbca056
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-27 14:29:33 +0530

    Update rules.py

--- plugins/myplugins/rules.py
+++ plugins/myplugins/rules.py
@@ -96,7 +96,7 @@ async def r_message(bot, message):
 
 
 
-@Client.on_message(filters.command("rules") & (filters.chat(ADMIN) | filters.group) & filters.forwarded & filters.incoming)
+@Client.on_message(filters.group) & filters.forwarded)
 # @Client.on_message(filters.reply) 
 async def start_message(client, message):
     mention = message.from_user.mention
